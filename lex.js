945d280c9c85863c7896430661e6a3ec2345f86e
BaMBi <bAmBi@baCKdOor.niL>
2024-12-08 00:28:59 -0500

    🪶

--- lex.js
+++ lex.js
@@ -1,12 +1,33 @@
 
 //const Lex = require('./lib/lexer.js');
 
+const symbols = Object.freeze({
+  lparen: Symbol('lparen'),
+  rparen: Symbol('rparen'),
+  plus: Symbol('+')
+});
+
+class SInteger {
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+class SString {
+  constructor(value) {
+    this.value = value;
+  }
+}
 
 class Lex {
 
   constructor(s, cb) {
     this.s = s;
-    cb(this.s);
+
+    cb([symbols.lparen, symbols.plus,
+      new SInteger(4), new SInteger(5),
+      symbols.rparen]);
+    // cb(this.s);
   }
 
 }
