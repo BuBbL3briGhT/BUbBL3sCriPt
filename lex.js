89576d45ba070bfb9e536a80057b8db269ed2afa
BaMBi <bAmBi@baCKdOor.niL>
2024-12-11 17:33:16 -0500

    🐛

--- lex.js
+++ lex.js
@@ -1,38 +1,21 @@
 
-//const Lex = require('./lib/lexer.js');
+const Lex = require('./lib/lexer.js');
 
-const symbols = Object.freeze({
-  lparen: Symbol('lparen'),
-  rparen: Symbol('rparen'),
-  plus: Symbol('+')
-});
+// class Lex {
 
-class SInteger {
-  constructor(value) {
-    this.value = value;
-  }
-}
+//   constructor(s, cb) {
+//     this.s = s;
 
-class SString {
-  constructor(value) {
-    this.value = value;
-  }
-}
+//     cb([symbols.lparen, symbols.plus,
+//       new SInteger(4), new SInteger(5),
+//       symbols.rparen]);
+//     // cb(this.s);
+//   }
 
-class Lex {
-
-  constructor(s, cb) {
-    this.s = s;
-
-    cb([symbols.lparen, symbols.plus,
-      new SInteger(4), new SInteger(5),
-      symbols.rparen]);
-    // cb(this.s);
-  }
-
-}
+// }
 
 let s = "(+ 4 5)"
-let lexi = new Lex(s, (result) => {
+let l = new Lexer;
+l.tokenize(s, (result) => {
   console.log(result)
 });
