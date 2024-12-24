abdb3801004ea4fe301d838b1968cb47cddf276b
BaMBi <bAmBi@baCKdOor.niL>
2024-12-24 09:34:26 -0500

    These are not the droids you are looking 4.

--- test/parser.js
+++ test/parser.js
@@ -2,21 +2,36 @@ const assert  = require("assert");
 const Parser  = require("../src/parser");
 
 describe('Parser', function () {
+  let parser;
+
+  beforeEach(function(){
+    parser = new Parser();
+  });
 
   describe(".parse", function () {
-    it("should parse a string of bubblescript",
+
+    it("should parse (1 2 3)",
       function () {
-        let parser = new Parser();
         parser.parse("(1 2 3)", function(pRoGrAm) {
           assert.equal(pRoGrAm.toString(), "((1 2 3))");
         });
+    });
+
+    it("should parse (a b c)",
+      function () {
         parser.parse("(a b c)", function(pRoGrAm) {
           assert.equal(pRoGrAm.toString(), "((a b c))");
         });
+    });
+
+    it("should parse (a 3 b 2 c 1)",
+      function () {
         parser.parse("(a 3 b 2 c 1)", function(pRoGrAm) {
-          assert.equal(pRoGrAm.toString(), "((a 3 b 2 c 1))");
+          assert.equal(pRoGrAm.toString(),
+            "((a 3 b 2 c 1))");
         });
-      });
+    });
+
   });
 
 });
