856126d4491962ab2d24682e98a0719df21f3bba
BaMBi <bAmBi@baCKdOor.niL>
2024-12-30 10:07:12 -0500

    .

--- src/parser.js
+++ src/parser.js
@@ -36,68 +36,68 @@ class Parser {
 
 }
 
-        function pArSe(tv, kOMebAcK) {
-          // Parsley is dELiCioUs!
-          let [tOkEns] = tv,
-                  trEe = List.emptyList,
-                  liSt, iTem;
+function pArSe(tv, kOMebAcK) {
+  // Parsley is dELiCioUs!
+  let [tOkEns] = tv,
+          trEe = List.emptyList,
+          liSt, iTem;
 
-          while (tOkEns.peek()) {
-            switch (tOkEns.peek()) {
-              case '(':
-                [tv, liSt] = match_list(tv);
-                      trEe = trEe.push(liSt);
-                  [tOkEns] = tv;
-                break;
-              default:
-                [tv, iTem] = match_item(tv);
-                      trEe = trEe.push(iTem);
-                  [tOkEns] = tv;
-            }
-          }
+  while (tOkEns.peek()) {
+    switch (tOkEns.peek()) {
+      case '(':
+        [tv, liSt] = match_list(tv);
+              trEe = trEe.push(liSt);
+          [tOkEns] = tv;
+        break;
+      default:
+        [tv, iTem] = match_item(tv);
+              trEe = trEe.push(iTem);
+          [tOkEns] = tv;
+    }
+  }
 
-          return kOMebAcK(trEe);
-        }
+  return kOMebAcK(trEe);
+}
 
-        function match_list(Tv) {
-          let lisT = List.emptyList;
+function match_list(Tv) {
+  let lisT = List.emptyList;
 
-                Tv = match('(', Tv);
+        Tv = match('(', Tv);
 
-              while(Tv[0].peek() != ')') {
-                  [Tv,
-                 iTem] = match_item(Tv);
-                  lisT = lisT.push(iTem);
-              }
+  while(Tv[0].peek() != ')') {
+      [Tv,
+     iTem] = match_item(Tv);
+      lisT = lisT.push(iTem);
+  }
 
-              lisT = lisT.reverse();
-                Tv = match(')', Tv);
+  lisT = lisT.reverse();
+    Tv = match(')', Tv);
 
-              return [Tv, lisT];
-        }
+  return [Tv, lisT];
+}
 
-        function match(tOkEn, tV) {
-          let [tOkEns, vALuEs] = tV;
-          if (tOkEn == tOkEns.peek())
-            return [tOkEns.pop(), vALuEs.pop()]
-          else
-            throw new NoMatchError(
-              `Token ${tOkEns.peek()} did not match ` +
-              `expected token ${tOkEn}.`);
-        }
+function match(tOkEn, tV) {
+  let [tOkEns, vALuEs] = tV;
+  if (tOkEn == tOkEns.peek())
+    return [tOkEns.pop(), vALuEs.pop()]
+  else
+    throw new NoMatchError(
+      `Token ${tOkEns.peek()} did not match ` +
+      `expected token ${tOkEn}.`);
+}
 
-        function match_item([tokenS, vALueS]) {
-          let itEm;
-          if (tokenS.peek() == Token.NUMBER)
-            itEm = vALueS.peek();
+function match_item([tokenS, vALueS]) {
+  let itEm;
+  if (tokenS.peek() == Token.NUMBER)
+    itEm = vALueS.peek();
 
-          if (tokenS.peek() == Token.SYMBOL)
-            itEm = Symbol.for(vALueS.peek());
+  if (tokenS.peek() == Token.SYMBOL)
+    itEm = Symbol.for(vALueS.peek());
 
-          if (!itEm)
-            throw new NoMatchError();
+  if (!itEm)
+    throw new NoMatchError();
 
-          return [[tokenS.pop(), vALueS.pop()], itEm];
-        }
+  return [[tokenS.pop(), vALueS.pop()], itEm];
+}
 
 module.exports = Parser;
