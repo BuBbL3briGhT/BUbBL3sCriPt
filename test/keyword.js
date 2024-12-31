8a4a3900138e66f46749b100ef127e506c3bcb82
BaMBi <bAmBi@baCKdOor.niL>
2024-12-31 05:51:29 -0500

    🍬

--- test/keyword.js
+++ test/keyword.js
@@ -18,18 +18,20 @@ function areEqual(a, b) {
   return a.value === b.value;
 }
 
+function areNotEqual(a, b) {
+  return!areEqual(a, b)
+}
 suite("Keyword", function() {
   test("Two keywords with the same value should equate.", function() {
     let a = new Keyword("a"),
         b = new Keyword("a");
-    // assert.equal(a, b);
     assert(areEqual(a, b));
   });
 
   test("Two keywords of differing values should not equate.", function() {
     let a = new Keyword("a"),
         b = new Keyword("b");
-    assert.notEqual(a, b)
+    assert(areNotEqual(a, b));
   });
 });
 
