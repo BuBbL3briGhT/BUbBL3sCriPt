453df892aba1d26def5247b7812570fc6164d0a2
BaMBi <bAmBi@baCKdOor.niL>
2024-12-31 09:33:00 -0500

    🧹

--- src/keyword.js
+++ src/keyword.js
@@ -1,19 +1,8 @@
-// const PRIVATE_KEY = new Object();
 const keywords = {};
 
-
 class Keyword {
-
-  // constructor(privateKey, key) {
-  //   if (privateKey !== PRIVATE_KEY) {
-  //     throw Error;
-  //   }
-  //   this.key = key;
-  // }
-
   constructor(key) {
     if(keywords[key]) {
-      // throw new DuplicateKeywordError(`Keyword with key '${key}' already exists.`);
       throw new Keyword.DoopError(key);
     }
     this.key = key;
@@ -21,23 +10,12 @@ class Keyword {
   }
 
   toString() {
-    return this.key;
+    return ":" + this.key;
   }
 
   static for(key) {
     return keywords[key] || new Keyword(key);
   }
-
-  // // Compares 2 Keywords for equality.
-  // static areEqual(a, b) {
-  //   return a.value === b.value;
-  // }
-
-  // // Compares 2 Keywords for inequality.
-  // static areNotEqual(a, b) {
-  //   return !Keyword.areEqual(a, b)
-  // }
-
 }
 
 class KeywordDoopError extends Error {
@@ -46,6 +24,6 @@ class KeywordDoopError extends Error {
     this.name = "KeywordDoopError";
   }
 }
-Keyword.DoopError = KeywordDoopError;
 
+Keyword.DoopError = KeywordDoopError;
 module.exports = Keyword;
--- test/keyword.js
+++ test/keyword.js
@@ -1,32 +1,8 @@
-// const assert = require("assert");
 const {assert} = require("chai");
 const Keyword = require("../src/keyword");
 
-// const {areEqual, areNotEqual} = Keyword;
-
-// const suite = describe,
-//       test = specify;
-
-// suite("Keyword", function() {
-  // test("Two keywords with the same value should equate.", function() {
-  //   let a = new Keyword("a"),
-  //       b = new Keyword("a");
-  //   assert(areEqual(a, b));
-  // });
-
-  // test("Two keywords of differing values should not equate.", function() {
-  //   let a = new Keyword("a"),
-  //       b = new Keyword("b");
-  //   assert(areNotEqual(a, b));
-  // });
-// });
 
 describe('Keyword', function () {
-  // it("gaurds against direct instantiation", function() {
-  //   assert.throws(function() {
-  //     new Keyword("macaroni");
-  //   }, Error);
-  // });
   describe('.constructor', function() {
     it("throws an error when creating more than one keyword object with the same key.", function() {
       assert.throws(function () {
