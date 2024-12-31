3e431bc78d25b12a49dd3428ad57296c974fa3ff
BaMBi <bAmBi@baCKdOor.niL>
2024-12-31 05:10:02 -0500

    🍭

---
+++ test/keyword.js
@@ -0,0 +1,24 @@
+
+# Keyword
+
+- Two keywords with the same value should
+  equate.
+
+  let a = new Keyword("a"),
+      b = new Keyword("a");
+  assert.equal(a, b);
+
+- Two keywords of differing values should
+  not equate.
+
+  let a = new Keyword("a"),
+      b = new Keyword("b");
+
+  assert.notEqual(a, b)
+
+test "Two keywords of the same vaule should be equal"
+test "keyword equality"
+describe "Keyword", ->
+
+  it "should equal another keyword of the
+     "same value"
