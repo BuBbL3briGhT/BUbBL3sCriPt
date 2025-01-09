5ee3bbb9762eb8e6e7e1c4f46a5b73affc8ab4d8
BaMBi <bAmBi@baCKdOor.niL>
2025-01-09 01:21:15 -0500

    🌭

--- src/eval.js
+++ src/eval.js
@@ -3,23 +3,89 @@ const Bubble = require("./bubble");
 const Keyword = require("./keyword");
 const parse = require("./parse.js");
 
-function eval(string) {
-  let it = parse(string);
-  console.log(it.peek().toString());
+const { map } = Bubble;
+
+const Ballon = undefined;
+
+function zing(q) {
+  return (p) => {
+    return q.call(this,
+      p.map(m => eVaL(this, m)))
+  }
 }
 
-function _eval(binding, expression) {
-  console.log(binding, expression);
+const bnd = {
+  "+": zing(function([a,[b]]) {
+    return a+b;
+  })
+}
 
-  // switch (exp && exp.constructor) {
-  //   case Symbol:
-  //     return exp.resolve(bnd)
-  //   case Quoted:
-  //     return exp.unquote();
-  //   default:
-  //     return exp;
-  // }
+function eval(string) {
+  return eVaL(parse(string));
+}
+
+function eVaL(bnd, xpr) {
+  switch (xpr && xpr.constructor) {
+    case Symbol:
+      return xpr.resolve(bnd)
+    case Bubble: {
+      let s = peek(xpr);
+      if (s instanceof Symbol) {
+        // debug('->', xpr.toString());
+        if (s.callPattern == 1) {
+          //  x or x/x or x.x/x
+          let q = eVaL(bnd, s);
+          if (q != s)
+            return eVaL(bnd,
+              push(pop(xpr), q));
+          else
+            return xpr;
+        } else /* send */ {
+          // call pattern 2
+          // x.x or x.x.x or x.x...
+          let q = resolveRoot(bnd, s)
+          if (!xpr.rest) {
+            return q[s.fn]()
+          }
+          try {
+            return q[s.fn](...xpr.rest.map(
+              function(a) {
+                return eVaL(bnd, a)
+              }).toArray())
+          } catch (e) {
+            console.log(s.fn);
+            throw e;
+          }
+        }
+      } else if (s instanceof Bubble) {
+        return eVaL(bnd,
+          push(pop(xpr), eVaL(bnd, s)))
+      } else if (s instanceof Fn) {
+        return s.call(bnd, pop(xpr));
+      } else if (s instanceof Function) {
+        return s.call(bnd, pop(xpr));
+      } else if (s instanceof Macro) {
+        return s.call(bnd, pop(xpr));
+      } else {
+        return undefined;
+      }
+    }
+    case Ballon:
+      return map(xpr, (a) => {
+        return eVaL(bnd, a)
+      });
+    case Fn:
+    // case Macro:
+      return each(xpr.body, (xpr) => {
+        return eVaL(bnd, xpr);
+      });
+    case Quoted:
+      return unquote(xpr);
+    default:
+      return xpr;
+  }
 };
+
 
 module.exports = eval;
 
