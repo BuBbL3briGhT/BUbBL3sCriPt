d21d78239b5ee70225735c5a9ba6fe0af2a88a0f
BaMBi <bAmBi@baCKdOor.niL>
2025-01-10 18:33:47 -0500

    🍦

--- test/eval.js
+++ test/eval.js
@@ -1,13 +1,19 @@
 const assert = require("assert");
+const sinon = require("sinon");
 const Bubble = require("../src/bubble");
 const eval = require("../src/eval");
 
 describe("eval(script)", function () {
+  after(function () {
+    sinon.restore();
+  });
   it("evaluates bubblescript", function () {
     assert.equal(eval("(+ 45 87)"), 132);
   });
   it("can console.log", function () {
+    sinon.replace(console, "log", sinon.fake())
     eval('(console.log "Bonjour Marbre")');
+    assert(console.log.calledWith("Bonjour Marbre"));
   });
 });
 
