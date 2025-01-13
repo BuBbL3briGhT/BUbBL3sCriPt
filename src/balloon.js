3591f6216f06cdc62d1d0b79ec1fe633df3c23d4
BaMBi <bAmBi@baCKdOor.niL>
2025-01-13 12:51:47 -0500

    🎈

--- src/balloon.js
+++ src/balloon.js
@@ -1,13 +1,13 @@
 
 class Balloon {
+  get isEmpty () { return this instanceof EmptyBalloon }
+
   static inflate () {
     return new EmptyBalloon();
   }
 }
 
-class EmptyBalloon extends Balloon {
-  get isEmpty () { return true }
-}
+class EmptyBalloon extends Balloon { }
 
 module.exports = Balloon;
 
