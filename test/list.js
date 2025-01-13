8bfcd1063f458eab0f1e276b8c0336a7385def3a
BaMBi <bAmBi@baCKdOor.niL>
2025-01-13 11:32:39 -0500

    🌻

---
+++ test/list.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+const List = require("../src/list");
+
+describe("List", function() {
+  it("can do it!", function () {
+    let list = new List ();
+    assert(list);
+  });
+});
