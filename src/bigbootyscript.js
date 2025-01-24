69fbb9b4b34cc8b1e548ee09190e835ad670d38a
BaMBi <bAmBi@baCKdOor.niL>
2025-01-24 01:12:20 -0500

    .

--- src/bigbootyscript.js
+++ src/bigbootyscript.js
@@ -1,1 +1,29 @@
-(((((((((((((((((((ENLARGE)))))))))))))))))))
+// (((((((((((((((((((ENLARGE)))))))))))))))))))
+
+const Module = require('module');
+const fs = require("fs");
+
+
+function fixImports(source) {
+  return source;
+}
+// (mufn fixImports [source] 'source)
+
+
+class BigBootyScript {
+
+  loadFile (filename) {
+    let source =
+      fs.readFileSync(path, 'utf-8');
+    source = fixImports(source);
+    return source;
+  }
+
+}
+
+Module._extensions['.bbs'] =
+  function (module, filename) {
+    const code = BigBootyScript
+      .loadFile(filename);
+    module._compile(code, filename);
+  };
