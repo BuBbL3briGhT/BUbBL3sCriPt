f19bb436008ade075fd965e1da610c2af9fbe8e9
BaMBi <bAmBi@baCKdOor.niL>
2025-01-28 01:58:05 -0500

    .

--- test/emoji.js
+++ test/emoji.js
@@ -7,5 +7,24 @@ let muyContenta = new Emoji("😃");
 console.log(muyContenta);
 let guiño = new Emoji("😉");
 console.log(guiño);
-let 🐒 = "Mona";
+let 🐒 = "mona";
 console.log(🐒);
+let consola = Object.create(console);
+consola.registro = consola.log;
+let 🐊 = "cocodrilo";
+consola.registro(🐊);
+consola.🪵 = consola.registro;
+let 🍇 = "uvas";
+consola.🪵(🍇);
+
+let 🪵 = consola.🪵;
+
+let 🧸 = "Oso de peluche";
+
+🪵(🧸);
+
+// let 🚜 = (🥓) => {
+//   🪵 (🥓, "->", eval(🥓));
+// }
+
+// 🚜("🧸");
