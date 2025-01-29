88c6eea31349a1bf617ce1d9d7db36eab5777e30
BaMBi <bAmBi@baCKdOor.niL>
2025-01-29 03:18:28 -0500

    .

--- src/akata.js
+++ src/akata.js
@@ -14,7 +14,7 @@ const { peek, pop, push, invert } = Bubble;
 // 🦒 🍑(estrate) {  briku 🍑🍑(tanpshi(estrate)); }
 function parse (string) { return double-parse ( tokenize ( string ) ) } ;
 
-// 🦓 🍑🍑(📺) {          🌻 [💰] = 📺, 🌳;
+// 🦓    🍑🍑         (  📺          )    {  🌻  [💰] = 📺, 🌳;
 function double-parse (  television  )    {  let [bag-of-gold] = television, bushy-tree;    guy on a bike speeding away from...
 
 
