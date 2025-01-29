2112d592bb0bde335e3619e6200d6ae4c47b1864
BaMBi <bAmBi@baCKdOor.niL>
2025-01-29 12:46:59 -0500

    .

--- src/akata.js
+++ src/akata.js
@@ -14,28 +14,29 @@ const { peek, pop, push, invert } = Bubble;
 // 🦒 🍑(estrate) {  briku 🍑🍑(tanpshi(estrate)); }
 function parse (string) { return double-parse ( tokenize ( string ) ) } ;
 
-      🦓       🍑🍑         (
-[Let] function double parse receive [a]
-📺        )      {   🌻   [   💰      ]  =
-television [and] do (Let (Get da money.) be
-             📺        ,
-equal to the television, and also [let there
-      🌳        ; 🚴... (shikī        ) {
-be] a BUSHY tree. While shikī is truthy do (
+
+       🦓         🍑🍑           (
+[Let]  function   double  parse  receive  [a]
+📺        )        {   🌻   [   💰      ]  =
+television  [and]  do (Let (Get da money.) be
+              📺        ,
+equal  to the television, and also [let there
+      🌳        ;  🚴... (shikī        ) {
+be] a BUSHY tree.  While shikī is truthy do (
        [
-[Let] (Get da first entry and put it in da
-📺         ,
-television and save the second entry to da
-🌳         ] =
-BUSHY tree.) be equal to [the result of
-                      inchaboi_yati  (
-calling da function] "inchaboi_yati" provid-
-               📺         ,       🌳
-ing it with da television and the BUSHY
-     );     [
-tree.  Let (extract the first entry into
-shikī]  =
-shikī.) equal [da value currently stored in
+[Let] (Get  da  first  entry and put it in da
+📺          ,
+television  and  save  the second entry to da
+🌳          ]  =
+BUSHY  tree.)  be  equal  to  [the  result of
+                       inchaboi_yati  (
+calling  da function] "inchaboi_yati" provid-
+                   📺          ,        🌳
+ing  it  with  da  television  and  the BUSHY
+      );      [
+tree.   Let  (extract  the  first  entry into
+shikī]   =
+shikī.)  equal  [da value currently stored in
     📺        ;} 🌸                  🌳
 da] television.) Briku (e.g. return) da BUSHY
     ;}
