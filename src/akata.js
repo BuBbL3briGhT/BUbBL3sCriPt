df814c061c0f3f486d3e836fd33b0d69341472f8
BaMBi <bAmBi@baCKdOor.niL>
2025-01-29 03:14:38 -0500

    🌽

--- src/akata.js
+++ src/akata.js
@@ -11,7 +11,12 @@ const { TOKEN_STRiNG, TOKEN_NUMBER, TOKEN_SYMBOL, TOKEN_KEYWORD } = tokenize;
 
 const { peek, pop, push, invert } = Bubble;
 
+// 🦒 🍑(estrate) {  briku 🍑🍑(tanpshi(estrate)); }
 function parse (string) { return double-parse ( tokenize ( string ) ) } ;
+
+function double-parse (  television  )    {  let [bag-of-gold] = television, bushy-tree; ....
+
+
 
 
 
