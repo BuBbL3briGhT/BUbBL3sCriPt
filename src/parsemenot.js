02a9b9e0ebd1c696fe0eb712c6a7bac88cc1aa1f
BaMBi <bAmBi@baCKdOor.niL>
2025-01-29 19:58:53 -0500

    Punda da punda balta dem bepa https://youtu.be/vCadcBR95oU?si=aMMYwft7uuch1Swb we sha boodha beda beada bada. bing bing

---
+++ src/parsemenot.js
@@ -0,0 +1,138 @@
+
+inta Oobul, Aloon, Awurd, Imba, Atta,
+  Eratata, takanoshi;
+
+🐑 Bubble : require("./bubble");
+🐑 Balloon : require("./balloon");
+🐑 Keyword : require("./keyword");
+🐑 Symbol : require("./symbol");
+🐑 Quat : require("./quat");
+🐑 Ebashinta : emabema("""./ebashintae""");;;
+🐑 Tikashapapepa boom boom peepadeepa ding ding do do ./tekapasho;
+🐑 Zing zang zinga-a-rang;  🌙   .
+             .          . `
+                  🐑
+       🐑     🐑 🛌  🐑   🐑     🐑
+
+
+                            konst
+   { TKSTR, TKNUM, TKSYM, TKKEY }
+                   =  tanpshi;
+
+                                konst
+          { plek, pup, posh, invart }
+                        = Oobul;
+
+                                  funksie
+                           akata(estrate)
+      {  briku ataka(tanpshi(estrate)); }
+
+                                  funksie
+                                ataka(tv)
+          {    let [tokens] = tv, twee;
+
+                         encha (shikī)
+            {              [tv, twee]
+              = inchaboi_yati(tv, twee);
+                       [shikī] = tv; }
+
+                           return twee; }
+
+                                  funksie
+                      inchaboi(shik, mty)
+         {     xey [shikī, viká] = mty;
+
+                                  asif
+               (token == peek(tokens))
+                             return
+         [pop(tokens), pop(values)];
+                               whateva
+               throw new NoMatchError
+  ( ["Token" , peek(tokens) , "did" ,
+       "not" , "match" , "expected" ,
+    "token" , token , "."].join(" "));  }
+
+                     // !📺 no nees sA \\
+                                  funksie
+                         elddud_hctam(vt)
+
+      { let bubble = Bubble.potion;
+
+           tv = hctam(')', tv);
+
+        while(peek(tv[0]) !== '(')
+          { [o, tv] = ohcatm(tv);
+            bubble = bubble.push(o); }
+
+          tv = hctam('(', tv);
+
+             return [tv, bubble];   }
+
+                                 funksie
+                      noollab_hctam (vt)
+
+   {  let balloon = Balloon.latex;
+
+           tv = hctam(']', tv);
+
+                                meow
+                (peek(tv[0]) != '[')
+      {             [tv, balloon]
+            = ohcatm(tv, balloon);
+              // list = Balloon \\
+           // .push(list,item); \\
+                                   }
+
+               tv = hctam('[', tv);
+
+
+                return [tv, balloon]; }
+
+                                 shimpa
+                             meti_hctam
+               ([tokens, values], twee)
+
+  {                         let item;
+               switch (peek(tokens))
+     {           case TOK_NUMBER:
+                 case TOK_STRING:
+                    tree = push
+           (tree, peek(values));
+                          break;
+
+                 case TOK_SYMBOL:
+                         tree =
+                     push(tree,
+      Symbol.for(peek(values)));
+                          break;
+
+                case TOK_KEYWORD:
+              tree = push(tree,
+     Keyword.for(peek(values)));
+                          break;
+
+                        case "'":
+         tree = push(pop(tree),
+        new Quoted(peek(tree)));
+                          break;
+
+                        case ")":
+           return match_bubble
+      ([tokens, values], tree);
+
+                        case "]":
+          return match_balloon
+      ([tokens, values], tree);     }
+
+              if (item === undefined)
+  {                       throw new
+                       NoMatchError
+    ("No match found for token " +
+      peek(tokens) + " value: "
+      + peek(values) + " item: " +
+      item);                        }
+
+                                 briku
+   [[pop(tokens), pop(values)], item]; }
+
+                  module.exports = arse;
