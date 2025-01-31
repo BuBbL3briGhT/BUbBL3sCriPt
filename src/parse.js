058c3436d049845451dafa8a930010c688727198
BaMBi <bAmBi@baCKdOor.niL>
2025-01-31 09:39:07 -0500

    resumption idol NFL

--- src/parse.js
+++ src/parse.js
@@ -65,13 +65,12 @@ AB3x  ꬰ ꬱ ꬲ ꬳ ꬴ ꬵ ꬶ ꬷ ꬸ ꬹ ꬺ ꬻ ꬼ ꬽ ꬾ ꬿ
 U+AB4x  ꭀ ꭁ ꭂ ꭃ ꭄ ꭅ ꭆ ꭇ ꭈ ꭉ ꭊ ꭋ ꭌ ꭍ ꭎ ꭏ
 U+AB5x  ꭐ ꭑ ꭒ ꭓ ꭔ ꭕ ꭖ ꭗ ꭘ ꭙ ꭚ ꭛ ꭜ
 r
-Latin Extended-E[1][2]
+
+Latin Extended-C[1]
 Official Unicode Consortium code chart (PDF)
   0 1 2 3 4 5 6 7 8 9 A B C D E F
-U+AB3x  ꬰ ꬱ ꬲ ꬳ ꬴ ꬵ ꬶ ꬷ ꬸ ꬹ ꬺ ꬻ ꬼ ꬽ ꬾ ꬿ
-U+AB4x  ꭀ ꭁ ꭂ ꭃ ꭄ ꭅ ꭆ ꭇ ꭈ ꭉ ꭊ ꭋ ꭌ ꭍ ꭎ ꭏ
-U+AB5x  ꭐ ꭑ ꭒ ꭓ ꭔ ꭕ ꭖ ꭗ ꭘ ꭙ ꭚ ꭛ ꭜ ꭝ ꭞ ꭟ
-U+AB6x  ꭠ ꭡ ꭢ ꭣ ꭤ ꭥ ꭦ ꭧ ꭨ ꭩ ꭪ ꭫
+U+2C6x  Ⱡ ⱡ Ɫ Ᵽ Ɽ ⱥ ⱦ Ⱨ ⱨ Ⱪ ⱪ Ⱬ ⱬ Ɑ Ɱ Ɐ
+U+2C7x  Ɒ ⱱ Ⱳ ⱳ ⱴ Ⱶ ⱶ ⱷ ⱸ ⱹ ⱺ ⱻ ⱼ ⱽ
 
 Latin Extended-D[1][2]
 Official Unicode Consortium code chart (PDF)
@@ -90,6 +89,43 @@ U+A7Cx  Ꟁ ꟁ Ꟃ ꟃ Ꞔ Ʂ Ᶎ Ꟈ ꟈ Ꟊ ꟊ Ɤ Ꟍ ꟍ
 U+A7Dx  Ꟑ ꟑ   ꟓ   ꟕ Ꟗ ꟗ Ꟙ ꟙ Ꟛ ꟛ Ƛ
 U+A7Ex
 U+A7Fx      ꟲ ꟳ ꟴ Ꟶ ꟶ ꟷ ꟸ ꟹ ꟺ ꟻ ꟼ ꟽ ꟾ ꟿ
+
+Latin Extended-E[1][2]
+Official Unicode Consortium code chart (PDF)
+  0 1 2 3 4 5 6 7 8 9 A B C D E F
+U+AB3x  ꬰ ꬱ ꬲ ꬳ ꬴ ꬵ ꬶ ꬷ ꬸ ꬹ ꬺ ꬻ ꬼ ꬽ ꬾ ꬿ
+U+AB4x  ꭀ ꭁ ꭂ ꭃ ꭄ ꭅ ꭆ ꭇ ꭈ ꭉ ꭊ ꭋ ꭌ ꭍ ꭎ ꭏ
+U+AB5x  ꭐ ꭑ ꭒ ꭓ ꭔ ꭕ ꭖ ꭗ ꭘ ꭙ ꭚ ꭛ ꭜ ꭝ ꭞ ꭟ
+U+AB6x  ꭠ ꭡ ꭢ ꭣ ꭤ ꭥ ꭦ ꭧ ꭨ ꭩ ꭪ ꭫
+
+Ethiopic[1][2]
+Official Unicode Consortium code chart (PDF)
+  0 1 2 3 4 5 6 7 8 9 A B C D E F
+U+120x  ሀ ሁ ሂ ሃ ሄ ህ ሆ ሇ ለ ሉ ሊ ላ ሌ ል ሎ ሏ
+U+121x  ሐ ሑ ሒ ሓ ሔ ሕ ሖ ሗ መ ሙ ሚ ማ ሜ ም ሞ ሟ
+U+122x  ሠ ሡ ሢ ሣ ሤ ሥ ሦ ሧ ረ ሩ ሪ ራ ሬ ር ሮ ሯ
+U+123x  ሰ ሱ ሲ ሳ ሴ ስ ሶ ሷ ሸ ሹ ሺ ሻ ሼ ሽ ሾ ሿ
+U+124x  ቀ ቁ ቂ ቃ ቄ ቅ ቆ ቇ ቈ   ቊ ቋ ቌ ቍ
+U+125x  ቐ ቑ ቒ ቓ ቔ ቕ ቖ   ቘ   ቚ ቛ ቜ ቝ
+U+126x  በ ቡ ቢ ባ ቤ ብ ቦ ቧ ቨ ቩ ቪ ቫ ቬ ቭ ቮ ቯ
+U+127x  ተ ቱ ቲ ታ ቴ ት ቶ ቷ ቸ ቹ ቺ ቻ ቼ ች ቾ ቿ
+U+128x  ኀ ኁ ኂ ኃ ኄ ኅ ኆ ኇ ኈ   ኊ ኋ ኌ ኍ
+U+129x  ነ ኑ ኒ ና ኔ ን ኖ ኗ ኘ ኙ ኚ ኛ ኜ ኝ ኞ ኟ
+U+12Ax  አ ኡ ኢ ኣ ኤ እ ኦ ኧ ከ ኩ ኪ ካ ኬ ክ ኮ ኯ
+U+12Bx  ኰ   ኲ ኳ ኴ ኵ     ኸ ኹ ኺ ኻ ኼ ኽ ኾ
+U+12Cx  ዀ   ዂ ዃ ዄ ዅ     ወ ዉ ዊ ዋ ዌ ው ዎ ዏ
+U+12Dx  ዐ ዑ ዒ ዓ ዔ ዕ ዖ   ዘ ዙ ዚ ዛ ዜ ዝ ዞ ዟ
+U+12Ex  ዠ ዡ ዢ ዣ ዤ ዥ ዦ ዧ የ ዩ ዪ ያ ዬ ይ ዮ ዯ
+U+12Fx  ደ ዱ ዲ ዳ ዴ ድ ዶ ዷ ዸ ዹ ዺ ዻ ዼ ዽ ዾ ዿ
+U+130x  ጀ ጁ ጂ ጃ ጄ ጅ ጆ ጇ ገ ጉ ጊ ጋ ጌ ግ ጎ ጏ
+U+131x  ጐ   ጒ ጓ ጔ ጕ     ጘ ጙ ጚ ጛ ጜ ጝ ጞ ጟ
+U+132x  ጠ ጡ ጢ ጣ ጤ ጥ ጦ ጧ ጨ ጩ ጪ ጫ ጬ ጭ ጮ ጯ
+U+133x  ጰ ጱ ጲ ጳ ጴ ጵ ጶ ጷ ጸ ጹ ጺ ጻ ጼ ጽ ጾ ጿ
+U+134x  ፀ ፁ ፂ ፃ ፄ ፅ ፆ ፇ ፈ ፉ ፊ ፋ ፌ ፍ ፎ ፏ
+U+135x  ፐ ፑ ፒ ፓ ፔ ፕ ፖ ፗ ፘ ፙ ፚ
+U+136x  ፠ ፡ ። ፣ ፤ ፥ ፦ ፧ ፨ ፩ ፪ ፫ ፬ ፭ ፮ ፯
+U+137x  ፰ ፱ ፲ ፳ ፴ ፵ ፶ ፷ ፸ ፹ ፺ ፻ ፼
+
 ʞ
 c
 ɔ
@@ -335,3 +371,8 @@ c
   }
 
 module.exports = parse;
+
+
+  ]i[ https://en.m.wikipedia.org/wiki/List_of_Unicode_characters
+  ]ii[ https://en.m.wikipedia.org/wiki/Latin_Extended-C
+  ]iii[ https://en.m.wikipedia.org/wiki/Latin_Extended-D
