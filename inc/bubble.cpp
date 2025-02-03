f47d3282d31aae210a27f36b7d7fe41c5545bb25
BaMBi <bAmBi@baCKdOor.niL>
2025-02-03 09:23:02 -0500

    .

--- inc/bubble.cpp
+++ inc/bubble.cpp
@@ -2,49 +2,71 @@
 #include <stdlib.h>
 
 struct Bubble {
-    int o;
+    void* o;
     struct Bubble* oo;
 };
 
+struct Bubble* blowBubble(void*);
+struct Bubble* blowBubble(struct Bubble*, void*);
 struct Bubble* blowBubble(int);
 struct Bubble* blowBubble(struct Bubble*, int);
-struct Bubble* popBubble(struct Bubble*);
-void           printBubbles(struct Bubble*);
+void* popBubble(struct Bubble*);
+void printBubbles(struct Bubble*);
 
 int main() {
+
+  int z = 9;
+  void* a = &z;
+  // printf("%d ", *(int *)a);
+
     struct Bubble* o;
-    o = blowBubble(1);
-    o = blowBubble(o, 2);
-    o = blowBubble(o, 3);
+    o = blowBubble(8);
+    // printf("%d ", *(int *)o->o);
+    // printf("%d ", *(int *)o->o);
+    // o = blowBubble(o, 2);
+    // o = blowBubble(o, 3);
     /* o = blowBubble(o, "apple"); */
     /* o = blowBubble(o, "orange"); */
     /* o = blowBubble(o, "🍌"); */
 
     printf("Bubbles: ");
-    printBubbles(o);
+    // printBubbles(o);
+    // o = blowBubble(8);
+    printf("%d ", *(int *)o->o);
+    printf("\n");
 
     return 0;
 }
 
-struct Bubble* blowBubble(int o) {
+struct Bubble* blowBubble(void* o) {
     struct Bubble* bubble = (struct Bubble*)malloc(sizeof(struct Bubble));
+    // printf("%d ", *(int *)o);
     bubble->o = o;
+    // printf("%d ", *(int *)bubble->o);
     bubble->oo = NULL;
     return bubble;
 }
 
-struct Bubble* blowBubble(struct Bubble* oo, int o) {
+struct Bubble* blowBubble(struct Bubble* oo, void* o) {
   struct Bubble* bubble = blowBubble(o);
   bubble->oo = oo;
   return bubble;
 }
 
+struct Bubble* blowBubble(int o) {
+  return blowBubble(&o);
+}
+
+struct Bubble* blowBubble(struct Bubble* oo, int o) {
+  return blowBubble(oo, &o);
+}
 /* struct Bubble* blowBubbles(); */
 
 void printBubbles(struct Bubble* oo) {
+    // printf("%d ", *(int *)oo->o);
     struct Bubble* o = oo;
     while (o != NULL) {
-        printf("%d ", o->o);
+        printf("%d ", *(int *)o->o);
         o = o->oo;
     }
     printf("\n");
