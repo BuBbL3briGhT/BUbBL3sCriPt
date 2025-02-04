3c190ca6794b2c90eca873b4fddb8a47ac3ed590
BaMBi <bAmBi@baCKdOor.niL>
2025-02-04 13:31:21 -0500

    .

--- fundy.c
+++ fundy.c
@@ -1,11 +1,28 @@
 # include <stdio.h>
 
-void* blow() {
-  return 0;
+union Bubble {
+   int i;
+};
+
+union Bubble blowBubble() {
+  union Bubble o;
+  return o;
+}
+
+union Bubble blowIntBubble(int i) {
+  union Bubble o;
+  o.i = i;
+  return o;
 }
 
 int main() {
-  void* o = blow();
-  printf("by Uchiha cb\n");
+  union Bubble o;
+
+  o = blowBubble();
+  printf("%d \n", o.i);
+
+  o = blowIntBubble(7);
+  printf("%d \n", o.i);
+
   return 0;
 }
