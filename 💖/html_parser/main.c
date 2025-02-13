913994b177f1df8adb20d22539f28ce4d71bd701
BaMBi <bAmBi@baCKdOor.niL>
2025-02-13 17:52:14 -0500

    file_read

--- 💖/html_parser/main.c
+++ 💖/html_parser/main.c
@@ -1,8 +1,28 @@
-
-
 #include "common.h"
 #include "scanner.h"
 #include "debug.h"
+
+char* read_file(const char* name) {
+  FILE* f;
+  f = fopen(name, "r");
+
+  if (f == NULL) {
+    perror("Failed to open the file.");
+    return NULL;
+  }
+
+  fseek(f, 0, SEEK_END);
+  long len = ftell(f);
+  fseek(f, 0, SEEK_SET);
+
+  char* file_mem = (char*)calloc(len, sizeof(char));
+
+  size_t el_read = fread(file_mem, sizeof(char), len, f);
+
+  fclose(f);
+
+  return file_mem;
+}
 
 int main() {
   char* source = read_file("test.html");
