fdb7b2b956695251c10d73ecf73760796ffee6e2
BaMBi <bAmBi@baCKdOor.niL>
2025-05-23 03:05:47 -0400

    Most of the test sweet is passing, only 14 failing tests. 🦁

--- src/eval.js
+++ src/eval.js
@@ -1,15 +1,14 @@
 
-const Bubble = require("./bubble");
+const Stack = require("./stack");
 const Keyword = require("./keyword");
 const parse = require("./parse");
-const Balloon = require("./balloon");
+const List = require("./list");
 const Fn = require("./fn");
 const Macro = require("./macro");
 const Symbol = require("./symbol");
-// const Symbobl = require("./symbobl");
 const Quoted = require("./quoted");
 
-const { map, peek, pop, push, toArray } = Bubble;
+const { map, peek, pop, push, toArray } = Stack;
 
 function mkfn(q) {
   return (p) => {
@@ -220,7 +219,7 @@ const rootBinding = {
     alert(this.concat(msgs));
   },
   parse: mkfn(function([fierce]) {
-    return bubbleParse(fierce);
+    return stackParse(fierce);
   }),
   eVaL: mkfn(function([v]) {
     return eVaL(this, v[0]);
@@ -236,7 +235,7 @@ const rootBinding = {
   })
 }
 
-// Evaluate Bubblescript
+// Evaluate Stackscript
 function eval(script) {
   return parse(script)
     .map(function(expression) {
@@ -254,7 +253,7 @@ function eVaL(bnd, xpr) {
     case Symbol:
       // debug('->', xpr.resolve(bnd));
       return xpr.resolve(bnd)
-    case Bubble: {
+    case Stack: {
       // console.log("🧀");
       let s = peek(xpr);
       // debug('->', s instanceof Symbol);
@@ -300,7 +299,7 @@ function eVaL(bnd, xpr) {
             throw e;
           }
         }
-      } else if (s instanceof Bubble) {
+      } else if (s instanceof Stack) {
         return eVaL(bnd,
           push(pop(xpr), eVaL(bnd, s)))
       } else if (s instanceof Fn) {
@@ -313,8 +312,8 @@ function eVaL(bnd, xpr) {
         return undefined;
       }
     }
-    case Balloon:
-      return Balloon.map(xpr, (a) => {
+    case List:
+      return List.map(xpr, (a) => {
         return eVaL(bnd, a)
       });
     case Fn:
--- test/eval.js
+++ test/eval.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 const sinon = require("sinon");
-const Bubble = require("../src/bubble");
-const Balloon = require("../src/balloon");
+const Stack = require("../src/stack");
+const List = require("../src/list");
 const eval = require("../src/eval");
 
 describe("eval(script)", function () {
@@ -16,10 +16,10 @@ describe("eval(script)", function () {
     eval('(console.log "Bonjour Marbre")');
     assert(console.log.calledWith("Bonjour Marbre"));
   });
-  it("evaluates a balloon with ease", function () {
+  it("evaluates a vector with ease", function () {
     let result = eval("[1 2 3]");
     // console.log(result);
-    assert(result instanceof Balloon);
+    assert(result instanceof List);
   });
 });
 
