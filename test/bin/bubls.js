4275db38d756b0556ceace40e36823e149c56b2c
BaMBi <bAmBi@baCKdOor.niL>
2025-05-23 10:38:02 -0400

    Cleanup 🎨

--- test/bin/bubls.js
+++ test/bin/bubls.js
@@ -11,47 +11,4 @@ describe('bin/bubls', () => {
       done();
     });
   });
-  // it('Provide an interactive prompt', (done) => {
-  //   exec('bin/bubls', (error, stdout, stderr) => {
-  //     expect(error).to.be.null;
-  //     // expect(stdout.trim()).to.equal('Hello, world!');
-  //     // expect(stderr).to.be.empty;
-  //     done();
-  //   });
-  // });
-  // it('Run a hello world program', (done) => {
-  //   exec('echo "(console.log \"Hello World\")" | bin/bubls', (error, stdout, stderr) => {
-  //     expect(error).to.be.null;
-  //     expect(stdout.trim()).to.equal('Hello, world!');
-  //     expect(stderr).to.be.empty;
-  //     done();
-  //   });
-  // });
 });
-
-// describe('Shell Commands', () => {
-//     it('should execute a command and return the correct output', (done) => {
-//         exec('echo "Hello, world!"', (error, stdout, stderr) => {
-//             expect(error).to.be.null;
-//             expect(stdout.trim()).to.equal('Hello, world!');
-//             expect(stderr).to.be.empty;
-//             done();
-//         });
-//     });
-
-//     it('should execute a command and return the correct exit code', (done) => {
-//         exec('exit 0', (error) => {
-//           expect(error).to.be.null;
-//           done();
-//         });
-//     });
-
-//     it('should handle errors from the command', (done) => {
-//         exec('command_that_does_not_exist', (error, stdout, stderr) => {
-//             expect(error).to.be.an('Error');
-//             expect(error.code).to.equal(127);
-//             expect(stderr).to.not.be.empty;
-//             done();
-//         });
-//     });
-// });
