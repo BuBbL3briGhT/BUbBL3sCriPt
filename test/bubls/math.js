b0731195b1e00e2e0882e87b43c0956ff96fe51c
BaMBi <bAmBi@baCKdOor.niL>
2025-05-24 19:30:58 -0400

    🦔

--- test/bubls/math.js
+++ test/bubls/math.js
@@ -5,49 +5,49 @@ const { eval } = bubls;
 
 describe("bubls", function () {
   it("runs tests", function () {
-    assertEvalTo("(- 3 2)", 1)
+    assertEval("(- 3 2)", 1)
   });
 });
 
 describe("+", function () {
   it("sums two numbers", function () {
-    assertEvalTo("(+ 1 1)", 2);
+    assertEval("(+ 1 1)", 2);
   });
   it("sums one number", function () {
-    assertEvalTo("(+ 1)", 1);
+    assertEval("(+ 1)", 1);
   });
   it("sums three numbers", function () {
-    assertEvalTo("(+ 1 1 1)", 3);
+    assertEval("(+ 1 1 1)", 3);
   });
 });
 
 describe("+", function () {
   it("sums", function () {
-    assertEvalTo("(+ 1)", 1);
-    assertEvalTo("(+ 1 1)", 2);
-    assertEvalTo("(+ 1 1 1)", 3);
+    assertEval("(+ 1)", 1);
+    assertEval("(+ 1 1)", 2);
+    assertEval("(+ 1 1 1)", 3);
   });
 });
 
 describe("-", function () {
   it("subtracts", function () {
-    assertEvalTo("(- 1 1)"   , 0);
-    assertEvalTo("(- 2 1)"   , 1);
-    assertEvalTo("(- 3 1)"   , 2);
-    assertEvalTo("(- 1)"     , 1);
-    assertEvalTo("(- 1 1 1)", -1);
+    assertEval("(- 1 1)"   , 0);
+    assertEval("(- 2 1)"   , 1);
+    assertEval("(- 3 1)"   , 2);
+    assertEval("(- 1)"     , 1);
+    assertEval("(- 1 1 1)", -1);
   });
 });
 
 describe("*", function () {
   it("multiplies", function () {
-    assertEvalTo("(* 1 1)", 1);
-    assertEvalTo("(* 1 1 1)", 1);
-    assertEvalTo("(* 1 1 2)", 2);
-    assertEvalTo("(* 1 2 2)", 4);
-    assertEvalTo("(* 2 2 2)", 8);
-    assertEvalTo("(* 2 2)", 4);
-    assertEvalTo("(* 1 2)", 2);
+    assertEval("(* 1 1)", 1);
+    assertEval("(* 1 1 1)", 1);
+    assertEval("(* 1 1 2)", 2);
+    assertEval("(* 1 2 2)", 4);
+    assertEval("(* 2 2 2)", 8);
+    assertEval("(* 2 2)", 4);
+    assertEval("(* 1 2)", 2);
   });
 });
 
@@ -63,9 +63,3 @@ function assertEval(expression, expected) {
   assert.equal(eval(expression), expected,
     expression + " => " + expected);
 }
-
-function assertEvalTo(expression, expected) {
-  assert.equal(eval(expression), expected,
-    expression + " => " + expected);
-}
-
