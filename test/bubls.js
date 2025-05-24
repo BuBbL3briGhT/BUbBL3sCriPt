4e754b470983133f8635084d105393ddbb684c87
BaMBi <bAmBi@baCKdOor.niL>
2025-05-24 06:55:09 -0400

    Add sum tests for bubble script. 🍒 addition

--- test/bubls.js
+++ test/bubls.js
@@ -1,5 +1,6 @@
 const assert = require("assert");
 const bubls = require("../src/bubls");
+
 const { eval } = bubls;
 
 describe("bubls", function () {
@@ -8,3 +9,14 @@ describe("bubls", function () {
   });
 });
 
+describe("+", function () {
+  it("sums two numbers", function () {
+    assert.equal(2, eval("(+ 1 1)"));
+  });
+});
+
+describe("+", function () {
+  it("sums", function () {
+    assert.equal(2, eval("(+ 1 1)"));
+  });
+});
