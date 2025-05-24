a1d79766b86afef5e5587ca873e5db4bfca4f85f
BaMBi <bAmBi@baCKdOor.niL>
2025-05-24 06:59:09 -0400

    Failing test for our niave addition implementation. 🍏

--- test/bubls.js
+++ test/bubls.js
@@ -13,10 +13,18 @@ describe("+", function () {
   it("sums two numbers", function () {
     assert.equal(2, eval("(+ 1 1)"));
   });
+  it("sums one number", function () {
+    assert.equal(1, eval("(+ 1)"));
+  });
+  it("sums three numbers", function () {
+    assert.equal(3, eval("(+ 1 1 1)"));
+  });
 });
 
 describe("+", function () {
   it("sums", function () {
+    assert.equal(1, eval("(+ 1)"));
     assert.equal(2, eval("(+ 1 1)"));
+    assert.equal(3, eval("(+ 1 1 1)"));
   });
 });
