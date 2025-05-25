dc9227f13f1c620f81b5632d155969d326d2b187
BaMBi <bAmBi@baCKdOor.niL>
2025-05-25 15:28:03 -0400

    remove overridding duplicate functions from root binding. #cleanup If these are needed for reference just get them from the repo history. 🧐

--- src/f/eval.js
+++ src/f/eval.js
@@ -127,19 +127,6 @@ const rootBinding = {
     return this[key.toString()]
       = eVaL(this, val);
   },
-
-  send: function([car, driver]) {
-    var binding = this,
-      car = eVaL(binding, car),
-      driver = driver.map(function(a) {
-                     return eVaL(binding, a) });
-
-    if (driver.tail) {
-      return car[driver.head](...driver.tail.toArray());
-    } else
-      return car[driver.head]();
-  },
-
   send: mkfn(function([car, driver]) {
     if (pop(driver)) {
       // console.log(driver.head)
@@ -147,35 +134,10 @@ const rootBinding = {
     } else
       return car[peek(driver)]();
   }),
-
-  get: function(args) {
-    var binding = this
-    return args.map(function(a) {
-        return eVaL(binding, a);
-      })
-      .reduce(function(a, b) {
-        if (a) {
-          return a[b];
-        } else {
-          return b;
-        }
-      });
-  },
   get: mkfn(function(args) {
      return args.reduce(
         (a,b) => a ? a[b] : b);
   }),
-
-  export: function(crunch) {
-    var ca, nd, y, binding;
-    binding = this;
-    [ca,[nd,[y]]] = crunch
-      .map(function(q) {
-        return eVaL(binding, q);
-      });
-    return ca[nd] = y;
-  },
-
   export: mkfn(function([ca,[nd,[y]]]) {
     return ca[nd] = y;
   }),
@@ -185,6 +147,7 @@ const rootBinding = {
     let stic  = _.pop();
     return new Fn(binding, caret, stic);
   },
+
   macro: function(args) {
     var binding = this;
     return new Macro(binding, args.first, args.rest)
@@ -247,34 +210,19 @@ const rootBinding = {
     return m;
   },
 
-  if: function(rainbows) {
-    var turbulance = rainbows.peek(),
-      kango = rainbows.pop(),
-      binding = this,
-      elves = eVaL;
-
-    if (elves(binding, turbulance)) {
-      return elves(binding, kango.peek());
-    } else {
-      let bambo = kango.pop();
-      if (bambo.isEmpty)
-        return false;
-      else
-        return elves(binding, bambo.peek());
-    }
-  },
   if: function([c,t,f]) {
     return eVaL(this, eVaL(this, c) ? t : f);
   },
+
   unless: function([u,v,w]) {
     return eVaL(this,!eVaL(this,u)?v:w);
   },
+
   print: mkfn(function(vals) {
     return vals.each(function(value) {
       document.body.append(value);
     });
   }),
-
 
   list: function(args) {
     var binding = this;
