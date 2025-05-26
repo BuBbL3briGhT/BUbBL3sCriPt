0d316817d2510bda08a5c79e281a1373687613fd
BaMBi <bAmBi@baCKdOor.niL>
2025-05-26 15:40:07 -0400

    😉 * multiplies base test pass

--- src/f/base.js
+++ src/f/base.js
@@ -47,5 +47,9 @@ Base["/"] = function(a) {
   return a.reduce((a,b) => a/b);
 }
 
+Base["*"] = function(a) {
+  return a.reduce((a,b) => a*b);
+}
+
 console.debug("Base 57", Base);
 module.exports = Base;
