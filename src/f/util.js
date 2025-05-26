2c241f60c72f897990538a13156a0a1cebba026b
BaMBi <bAmBi@baCKdOor.niL>
2025-05-26 06:32:54 -0400

    . Doc string for Util.getStaticMethods. 🌴

--- src/f/util.js
+++ src/f/util.js
@@ -2,6 +2,10 @@
 // common and helpful utility methods used
 // through the project.
 class Util {
+
+  // Returns an array of static method
+  // names, as strings, from the passed in
+  // 'klass' object.
   static getStaticMethods(klass) {
     return  Object.getOwnPropertyNames(klass).filter(
       (prop) => typeof klass[prop] === 'function' && !Object.prototype.hasOwnProperty.call(klass.prototype, prop)
