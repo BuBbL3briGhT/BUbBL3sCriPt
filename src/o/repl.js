d5bf3078ca383edb58982d44c6c7054d3c84a956
BaMBi <bAmBi@baCKdOor.niL>
2025-05-30 11:18:00 -0400

    . repl constructor createRepl

--- src/o/repl.js
+++ src/o/repl.js
@@ -1,6 +1,21 @@
+const readline = require('readline');
+const Parinfer = require('parinfer'); // Assuming you have the parinfer module installed
+
 // Interactive Bubblescript repl
 class Repl {
-  //TODO: Implement class.
+  constructor () {
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  }
+}
+
+function createRepl() {
+  let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 }
 
 module.exports = Repl;
