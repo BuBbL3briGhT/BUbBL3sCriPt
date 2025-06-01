28f02b8fc9585c3057ddf1c513a39df5d1a81228
BaMBi <bAmBi@baCKdOor.niL>
2025-05-31 21:30:47 -0400

    . repl sketch

--- repl.js
+++ repl.js
@@ -3,24 +3,39 @@ const readline = require('readline');
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
-var input = "";
-var x_pos = 0;
-var y_pos = 0;
+var currentLine = "";
+var lines = [currentLine];
+var cursor_x = 0;
+var cursor_y = 0;
 
 process.stdin.on('keypress', (str, key) => {
   // console.log('Key pressed:', key);
   if (key.ctrl && key.name === 'c') {
     process.exit();
   }
-  input += key.name;
-  updateView():
+  currentLine += key.name;
+  updateView();
 });
 
 function updateView() {
   clear();
-  console.log(input);
+  for (let line of lines) {
+    console.log(line);
+  }
 }
 
+function clear() {
+  clearLines(lines.length);
+}
+
+function clearLines(n) {
+  for (let i = 0; i < n; i++) {
+    const y = i === 0 ? null : -1;
+    process.stdout.moveCursor(0, y);
+    process.stdout.clearLine(1);
+    process.stdout.cursorTo(0);
+  }
+}
 
 process.stdin.resume();
 
