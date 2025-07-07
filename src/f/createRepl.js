58156e00e91521a3009a04e8f7dfb5fd930e072f
BaMBi <bAmBi@baCKdOor.niL>
2025-07-07 11:30:53 -0400

    .

--- src/f/createRepl.js
+++ src/f/createRepl.js
@@ -4,11 +4,11 @@ const parse = require("../../src/f/parse");
 
 function createRepl(rl) {
   rl.on('line', (line) => {
-    try {
-      const tokens = parse(line);
-    } catch (error) {
-    }
-    console.log(tokens);
+    // try {
+    //   const tokens = parse(line);
+    // } catch (error) {
+    // }
+    // console.log(tokens);
     // Use Parinfer to process the line
     const processedLine = Parinfer.indentMode(line, { // Use indentMode or parenMode as needed
       // Parinfer options
@@ -16,7 +16,7 @@ function createRepl(rl) {
 
     // Display the processed line (or handle errors/changes)
     console.log(processedLine.text);
-    console.log(processedLine);
+    // console.log(processedLine);
     eval(processedLine.text);
     // eval(line);
 
