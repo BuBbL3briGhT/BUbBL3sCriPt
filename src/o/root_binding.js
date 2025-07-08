f7c6ed00c10939ff70ad12b5fc307cc32952c32b
BaMBi <bAmBi@baCKdOor.niL>
2025-07-07 21:56:38 -0400

    unless

--- src/o/root_binding.js
+++ src/o/root_binding.js
@@ -113,12 +113,23 @@ const rootBinding = {
   },
 
   if: function([c,t,f]) {
-    return _eval(this, _eval(this, c) ? t : f);
+    return _eval(this,
+      _eval(this, c) ? t : f);
   },
 
-  unless: function([u,v,w]) {
-    return _eval(this,!_eval(this,u)?v:w);
+  // unless: function([u,v,w]) {
+  //   return _eval(this,
+  //     ! _eval(this, u) ? v : w);
+  // },
+
+  unless: function([c,f,t]) {
+    return _eval(this,
+      _eval(this, c) ? t : f);
   },
+
+  // unless: function(...args) {
+  //   return !this.if(...args);
+  // },
 
   print: mkfn(function(vals) {
     return vals.each(function(value) {
