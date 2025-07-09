c71aa7e96e1d61c01fb0a79fa2ba9ea986cd54e5
BaMBi <bAmBi@baCKdOor.niL>
2025-07-09 18:24:22 -0400

    comment test for now

--- test/bubls/bubls.js
+++ test/bubls/bubls.js
@@ -24,11 +24,11 @@ describe("send", function () {
   });
 });
 
-describe("push", function () {
-  it.only("pushes", function () {
-    eval("(push [1 2]) 3");
-  });
-});
+// describe("push", function () {
+//   it.only("pushes", function () {
+//     eval("(push [1 2]) 3");
+//   });
+// });
 
 describe("puts", function () {
   before(function () {
