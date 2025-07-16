3b67270e7d8600ea47969a3a5fedd63a6d573757
BaMBi <bAmBi@baCKdOor.niL>
2025-07-16 15:17:56 -0400

    . linked_list cleanup fixes

--- src/o/linked_list.js
+++ src/o/linked_list.js
@@ -2,17 +2,17 @@ let emptyLinkedList;
 
 class LinkedList {
 
-  static listOpenChar = "";
-  static listCloseChar = "";
+  listOpenChar = "";
+  listCloseChar = "";
 
   static get empty() { return emptyLinkedList; }
 
   get isEmpty() { return false; }
-  get first() { return peek(this); }
-  get rest() { return pop(this); }
-  get next() { return peek(pop(this)); }
-  get last() { return !pop(this).isEmpty ?
-      pop(this).last : peek(this); }
+  get first() { return this.peek(); }
+  get rest() { return this.pop(); }
+  get next() { return this.pop().peek(); }
+  get last() { return !this.pop().isEmpty ?
+      this.pop().last : this.peek(); }
 
   // Create a linkedList.
   constructor(o, oo=this.empty) {
@@ -26,13 +26,6 @@ class LinkedList {
     }, 0);
   }
 
-  // static make(...elements) {
-  //   var head = this.empty;
-  //   for (let o of elements)
-  //     head = new this(o, head);
-  //   return head;
-  // }
-
   static make(...elements) {
     var head = this.empty;
     for (let o of elements)
@@ -40,29 +33,14 @@ class LinkedList {
     return head;
   }
 
-  // static from(arrayLike, mapFn, thisArg) {
-  //   let array = Array.from(arrayLike, mapFn, thisArg);
-  //   return this.make(...array);
-  // }
-
   static from(arrayLike, mapFn, thisArg) {
     let array = Array.from(arrayLike, mapFn, thisArg);
     return this.make(...array);
   }
 
-  // static push(vector, element) {
-  //   return new this(element, vector);
-  // }
-
   push(element) {
     return new this.constructor(element, this);
   }
-
-  // static map(o, fn) {
-  //   if (o.isEmpty) return o;
-  //   return new this(fn(o.o),
-  //     map(o.oo, fn));
-  // }
 
   map(fn) {
     if (this.isEmpty) return this;
@@ -70,15 +48,7 @@ class LinkedList {
       this.oo.map(fn));
   }
 
-  // static get(o,i) { return peek(skip(o,i)); }
-
   get(i) { return this.skip(i).peek(); }
-
-  // static skip(o, count) {
-  //   if (count)
-  //     return skip(pop(o), --count);
-  //   return o;
-  // }
 
   skip(count) {
     if (count)
@@ -86,30 +56,12 @@ class LinkedList {
     return this;
   }
 
-  // static peek(o) { return o.o; }
-  // static pop(o) { return o.oo; }
-
   peek() { return this.o; }
   pop() { return this.oo; }
-
-  // static shift(o) {
-  //   return invert(pop(invert(o)));
-  // }
 
   shift() {
     return this.invert().pop().invert();
   }
-
-  // static invert(o) {
-  //   if (o.isEmpty)
-  //     return o;
-
-  //   // oo (accumulator), o (currentElement)
-  //   return reduce(pop(o),
-  //     (accumulator, currentElement) => {
-  //       return push(accumulator, currentElement);
-  //     }, make(peek(o)));
-  // }
 
   invert() {
     if (this.isEmpty)
@@ -121,38 +73,12 @@ class LinkedList {
       }, this.constructor.make(this.peek()));
   }
 
-  // static conj(targetLinkedList, sourceLinkedList) { // o -> targetLinkedList, oo -> sourceLinkedList
-  //   // oo (accumulator), o (currentElement)
-  //   return sourceLinkedList.reduce(function(accumulator, currentElement) {
-  //     return accumulator.push(currentElement);
-  //   }, targetLinkedList); // o -> targetLinkedList
-  // }
-
   conj(sourceLinkedList) { // o -> targetLinkedList, oo -> sourceLinkedList
     // oo (accumulator), o (currentElement)
     return sourceLinkedList.reduce(function(accumulator, currentElement) {
       return accumulator.push(currentElement);
     }, this); // o -> targetLinkedList
   }
-
-  // static toString(o, listOpenChar, listCloseChar, join) {
-  //   if (o.isEmpty) return listOpenChar + listCloseChar;
-
-  //   let format = (o) => {
-  //     switch (typeof o) {
-  //       case "string":
-  //         return '"' + o + '"';
-  //       case "symbol":
-  //         return Symbol.keyFor(o);
-  //       default:
-  //         return o.toString();
-  //     }
-  //   }
-
-  //   return listOpenChar +
-  //     reduce(map(o, format), join)
-  //        + listCloseChar;
-  // }
 
   toString() {
     if (this.isEmpty) return this.listOpenChar + this.listCloseChar;
@@ -177,31 +103,10 @@ class LinkedList {
     return formattedElement + " " + accumulatedString;
   });
 
-  // static toArray(o) {
-  //   return reduce(o, (array, currentElement) => {
-  //     array.push(currentElement); return array; }, []);
-  // }
-
   toArray() {
     return this.reduce((array, currentElement) => {
       array.push(currentElement); return array; }, []);
   }
-
-  // static reduce(o, fn, memo) {
-  //   if (o.isEmpty) return memo;
-
-  //   let oo = pop(o);
-  //   if (oo)
-  //     if (memo !== undefined)
-  //       return reduce(oo, fn,
-  //         fn(memo, peek(o)))
-  //     else
-  //       return reduce(oo, fn, peek(o));
-  //   else if(memo !== undefined)
-  //     return fn(memo, peek(o));
-  //   else
-  //     return peek(o);
-  // }
 
   reduce(fn, memo) {
     if (this.isEmpty) return memo;
@@ -219,38 +124,11 @@ class LinkedList {
       return this.peek();
   }
 
-  // static each(o, fn) {
-  //   let oo = fn(peek(o));
-  //   if (pop(o).isEmpty) return oo;
-  //   return each(pop(o), fn);
-  // }
-
-  static this.each(fn) {
+  each(fn) {
     let oo = fn(this.peek());
     if (this.pop().isEmpty) return oo;
     return this.pop().each(fn);
   }
-
-  //// Members Only ¥ ////
-
-  count() { return count(this); }
-  conj(sourceLinkedList) { return this.constructor.conj(this, sourceLinkedList); } // oo -> sourceLinkedList
-  each(fn) { return this.constructor.each(this, fn); }
-  get(i) { return this.constructor.get(this, i); }
-  invert() { return this.constructor.invert(this); }
-  map(fn) { return this.constructor.map(this, fn); }
-  peek() { return this.constructor.peek(this); }
-  pop() { return this.constructor.pop(this); }
-  push(element) { return this.constructor.push(this, element); } // o -> element
-  reduce(fn, memo) {
-    return this.constructor.reduce(this, fn, memo); }
-  shift() { this.constructor.shift(this); }
-  skip(n) { this.constructor.skip(this, n) };
-  toString() { return toString(this, this.listOpenChar, this.listCloseChar,
-    (accumulatedString, formattedElement) => {
-      return formattedElement + " " + accumulatedString;
-    }); }
-  toArray() { return this.constructor.toArray(this); }
 
   *[Symbol.iterator]() {
     let currentNode = this;
@@ -264,15 +142,6 @@ class LinkedList {
     }
   }
 }
-
-// const _toString = LinkedList.toString;
-// const toString = _toString.bind(LinkedList);
-// toString.toString = _toString;
-// LinkedList.toString = toString;
-
-const {count, conj, each, get, invert,
-  map, make, peek, pop, push, reduce,
-  shift, skip, toArray } = LinkedList
 
 class EmptyLinkedList {
   get isEmpty() { return true; }
