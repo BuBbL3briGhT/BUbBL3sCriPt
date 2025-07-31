3a24b4d576c42e20f73958f801ed64f9f2bc25a3
BaMBi <bAmBi@baCKdOor.niL>
2025-07-31 16:19:17 -0400

    . eval test

--- test/f/eval.js
+++ test/f/eval.js
@@ -1,11 +1,9 @@
 const assert = require("assert");
 const sinon = require("sinon");
-const List = require("../../src/o/list");
-const Vector = require("../../src/o/vector");
-const eval = require("../../src/f/eval");
 
-const parse = require("../../src/f/parse");
-const rootBinding = require("../../src/o/root_binding");
+const { List, Vector, eval: _eval, parse,
+  rootBinding } =
+  require("../../src/BubbleScript");
 
 describe("eval(script)", function () {
 
@@ -15,25 +13,25 @@ describe("eval(script)", function () {
 
   it("runs script top to bottom", function () {
     sinon.replace(console, "log", sinon.fake())
-    eval("(muf puts (fn [a] (console.log a)))\n(puts 1)\n(puts 2)\n(puts 3)")
-    // eval("(puts 3)\n(puts 2)\n(puts 1)\n(muf puts (fn [a] (console.log a)))"); // fyi: Pass with this under reverse execution.
+    _eval("(muf puts (fn [a] (console.log a)))\n(puts 1)\n(puts 2)\n(puts 3)")
+    // _eval("(puts 3)\n(puts 2)\n(puts 1)\n(muf puts (fn [a] (console.log a)))"); // fyi: Pass with this under reverse execution.
     assert(console.log.calledWith(1));
     assert(console.log.calledWith(2));
     assert(console.log.calledWith(3));
   });
 
   it("evaluates listcript", function () {
-    assert.equal(eval("(+ 45 87)"), 132);
+    assert.equal(_eval("(+ 45 87)"), 132);
   });
 
   it("can console.log", function () {
     sinon.replace(console, "log", sinon.fake())
-    eval('(console.log "Bonjour Marbre")');
+    _eval('(console.log "Bonjour Marbre")');
     assert(console.log.calledWith("Bonjour Marbre"));
   });
 
   it("evaluates a vector with ease", function () {
-    let result = eval("[1 2 3]");
+    let result = _eval("[1 2 3]");
     // console.log(result);
     assert(result instanceof Vector);
   });
@@ -42,12 +40,12 @@ describe("eval(script)", function () {
     let bnd = Object.create(rootBinding);
     let ast = parse("(muf 🐒 (macro [] °(puts \"Monkey\")))");
     assert.equal(ast.toString(), "((muf 🐒 (macro [] °(puts \"Monkey\"))))");
-    ast.evalEach(bnd)
-    let fn = parse("(fn [] (🐒))").evalEach(bnd);
+    ast.eval(bnd)
+    let fn = parse("(fn [] (🐒))").eval(bnd);
     assert.equal(fn.body.toString(), "((🐒))");
-    fn.body.evalEach(bnd);
+    fn.body.eval(bnd);
     assert.equal(fn.body.toString(), "((puts \"Monkey\"))");
-    fn.body.evalEach(bnd);
+    fn.body.eval(bnd);
     assert.equal(fn.body.toString(), "((puts \"Monkey\"))");
   });
 
@@ -56,10 +54,10 @@ describe("eval(script)", function () {
     bnd.puts = null;
     let ast = parse("(muf 🐒 (macro [🐸 🐷 🦎] (list °puts (list °+ 🐸 🐷 🦎)) (list °puts (+ 🐸 🐷 🐷) 🦎)))");
     assert.equal(ast.toString(), "((muf 🐒 (macro [🐸 🐷 🦎] (list °puts (list °+ 🐸 🐷 🦎)) (list °puts (+ 🐸 🐷 🐷) 🦎))))");
-    ast.evalEach(bnd)
-    let fn = parse("(fn [🪻] (* 6 9) (🐒 1 2 🪻) (+ 3 4))").evalEach(bnd);
+    ast.eval(bnd)
+    let fn = parse("(fn [🪻] (* 6 9) (🐒 1 2 🪻) (+ 3 4))").eval(bnd);
     assert.equal(fn.body.toString(), "((* 6 9) (🐒 1 2 🪻) (+ 3 4))");
-    fn.body.evalEach(bnd);
+    fn.body.eval(bnd);
     assert.equal(fn.body.toString(), "((* 6 9) (puts (+ 1 2 🪻)) (puts 5 🪻) (+ 3 4))");
   });
 });
