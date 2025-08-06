e64e389c116ae4e6944588acaabe50136134c0ac
BaMBi <bAmBi@baCKdOor.niL>
2025-08-06 01:53:24 -0400

    . cleanup improve

--- src/BubbleScript.js
+++ src/BubbleScript.js
@@ -158,49 +158,24 @@ class AbstractList {
     if (this.isEmpty)
       return this;
     if (value == this.head)
-      return this.constructor.emptyList || this.constructor.emptyVector;
+      return this.constructor.make();
     else
       return new this.constructor(this.head, this.tail.until(value));
   }
 
   split(value) {
-    let result = this.constructor.make();
-    let b = this.find(value);
-    if (b)
-      result = result.push(b.pop());
-    result = result.push(this.until(value));
-    return result;
-  }
-
-  split(value) {
-    let result = this.constructor.make();
-    let b = this.find(value);
-    if (b) {
-      b = b.pop();
-      if (b.find(value))
-        result = b.split(value);
+    let result;
+    let sub = this.find(value);
+    if (sub) {
+      sub = sub.pop();
+      if (sub.find(value))
+        result = sub.split(value);
       else
-        result = result.push(b);
+        result = this.constructor.make(sub);
     }
     result = result.push(this.until(value));
     return result;
   }
-
-  // split(delimiter) {
-  //   if (this.isEmpty) {
-  //     return this.constructor.make(this);
-  //   } else {
-  //     let tail = this.tail.split(delimiter);
-  //     if (this.head == delimiter)
-  //       return tail.push(this.head)
-  //     if (tail.head == delimiter) {
-  //       return tail.pop().push(this.constructor.make(this.head));
-  //     }
-  //     // console.log(tail);
-  //     // console.log(tail);
-  //     return tail.pop().push(tail.head.push(this.head));
-  //   }
-  // }
 
   *[Symbol.iterator]() {
     let currentNode = this;
