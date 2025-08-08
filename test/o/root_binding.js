5631493754847681c2a2ef74f547135584bc215f
BaMBi <bAmBi@baCKdOor.niL>
2025-08-07 20:41:48 -0400

    .

--- test/o/root_binding.js
+++ test/o/root_binding.js
@@ -3,11 +3,14 @@ const { rootBinding, List} = require("../../src/BubbleScript");
 
 describe("rootBinding", function () {
    describe("get", function () {
-     it.only("gets from object", function () {
+     it("gets from object", function () {
        let obj = { name: { first: "Kermit" }};
        assert.equal(rootBinding
          .get(List.make(obj, "name", "first")),
          "Kermit");
+       assert.deepEqual(rootBinding
+         .get(List.make(obj, "name")),
+         { first: "Kermit" });
      });
    });
 });
