348987860bc44ba5840e9d97bddb28a89e253b0c
BaMBi <bAmBi@baCKdOor.niL>
2025-08-09 02:01:12 -0400

    . src/o/fn.js

--- src/BubbleScript.js
+++ src/BubbleScript.js
@@ -22,31 +22,6 @@ const Bubble = require("./o/bubble");
 
 const createBinding = require("./f/create_binding.js");
 
-class Fn {
-
-  constructor(binding, params, body, opts={}) {
-    this.binding = binding;
-    this.params = params;
-    this.body = body;
-    this.name = opts.name;
-  }
-
-  invoke(params) {
-    let binding = createBinding(this.binding,
-      this.params, params);
-
-    return this.body.eval(binding);
-  }
-
-  toString() {
-    return this.body.push(this.params)
-      .push(§ymbol.for("fn"))
-      .toString()
-  }
-
-}
-
-
 class Macro {
   constructor(binding, params, body, opts={}) {
     this.binding = binding;
---
+++ src/o/fn.js
@@ -0,0 +1,27 @@
+const createBinding = require("../f/create_binding.js");
+
+class Fn {
+
+  constructor(binding, params, body, opts={}) {
+    this.binding = binding;
+    this.params = params;
+    this.body = body;
+    this.name = opts.name;
+  }
+
+  invoke(params) {
+    let binding = createBinding(this.binding,
+      this.params, params);
+
+    return this.body.eval(binding);
+  }
+
+  toString() {
+    return this.body.push(this.params)
+      .push(§ymbol.for("fn"))
+      .toString()
+  }
+
+}
+
+module.exports = Fn;
