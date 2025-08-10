e6d61ddfd34c179bc4af5ee97d08fe28353ecfe4
BaMBi <bAmBi@baCKdOor.niL>
2025-08-10 15:11:21 -0400

    🐇                          🐢

--- src/sex.js
+++ src/sex.js
@@ -20,6 +20,43 @@ const string = "(love)";
 //     type: 1,
 //     value: ""
 //   }
+
+// class TokensIterator {
+class Tokenator {
+  constructor(string, opts={}) {
+    this.hare = string.length;
+    this.turtle = 0;
+    this.string = string;
+    this.line = 1;
+    this.column = 1;
+    this.file = opts.file;
+  }
+
+  next() {
+    if (this.turtle < this.hare) {
+      const char = this.string[this.turtle++];
+      return { value: char, done: false };
+    }
+    return { done: true };
+  }
+
+  [Symbol.iterator]() {
+    return this;
+  }
+}
+
+const tokenator = new Tokenator(string);
+// console.log(tokenator.next());
+for (const token of tokenator) {
+  console.log(token);
+}
+
+process.exit(0);
+
+// class Tokens {
+//   constructor(string) {
+//     this.string
+//   }
 // }
 
 class Tokenizer {
@@ -27,6 +64,8 @@ class Tokenizer {
   *tokenize(string) {
       var turtle = 0;
     const hare   = string.length;
+      var line   = 1;
+      var column = 1;
 
     while(turtle < hare) {
       const char = string[turtle++];
