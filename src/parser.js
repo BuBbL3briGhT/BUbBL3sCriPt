23db10fc20af57d562bce06b6a92fd7db8961855
BaMBi <bAmBi@baCKdOor.niL>
2025-08-11 16:58:51 -0400

    🐇          🐢

--- src/parser.js
+++ src/parser.js
@@ -19,32 +19,101 @@ class Parser {
   }
 
   next() {
-    let expr; // expression
-    for (const token of this.tokens) {
+    const token = this.nextToken();
+    const o = token && this.parse(token);
+    return { value: o, done: !o };
+  }
 
-      switch (token.type) {
-        case TOK_SYMBOL:
-          expr = Ṣymbol.for(token.value);
-          break;
-        case TOK_NUMBER:
-          expr = token.value;
-          break;
-        default:
-          throw new Error("No match for token "
-            + JSON.stringify(token));
-      }
+  nextToken() {
+    return this.tokens.next().value;
+  }
 
-      // return { value: token, done: !token };
-      return { value: expr, done: !expr };
+  parse(token) {
+    let o;
+    switch (token.type) {
+      case TOK_SYMBOL:
+        o = Ṣymbol.for(token.value);
+        break;
+      case TOK_NUMBER:
+        o = token.value;
+        break;
+      case "(":
+        const list = List.make()
+          list.line = token.line;
+          list.column = token.column;
+          list.file = token.file;
+        o = this.parseList(list);
+        break;
+      default:
+        throw new TokenNoMatchError(token);
     }
-    // const token = this.tokens.next().value;
-    return { value: expr, done: !expr };
+
+    return o;
+  }
+
+  parseList(list) {
+    const token = this.nextToken();
+    if (token)
+      if (token.type === ')')
+        return list;
+      else
+        return this.parseList(list).push(this.parse(token));
+    else
+      throw new UnexpectedEndOfInputError();
   }
 
   [Symbol.iterator]() {
     return this;
   }
 }
+
+// throw new ParsingError("No matching ')' for list " + JSON.stringify(list.token));
+// throw new ParsingError(
+//   "No matching ')' for list" +
+//   list.token.line );
+
+
+
+// for (const token of this.tokens) {
+//   if (token.type === ')')
+//     return list;
+//   else
+//     list = list.push
+
+//   switch (token.type) {
+//     case ")":
+//     default:
+//       throw new TokenNoMatchError(token);
+//   }
+// }
+// }
+// for (const token of this.tokens) {
+//   switch (token.type) {
+//     case TOK_SYMBOL:
+//       o = Ṣymbol.for(token.value);
+//       break;
+//     case TOK_NUMBER:
+//       o = token.value;
+//       break;
+//     case "(":
+//       o = this.parseList();
+//       break;
+//     default:
+//       throw new TokenNoMatchError(token);
+//   }
+// }
+// }
+
+// class ParsingError extends Error {
+//   constructor(message, token) {
+//     super(message);
+//     this.name = "ParsingError";
+//     if (token) {
+//       // Ensure the message includes token details if a token is provided
+//       this.message = `${message} (at line ${token.line}, column ${token.column}, value: '${token.value}')`;
+//     }
+//   }
+// }
 
 const assert = require("assert");
 
@@ -63,6 +132,24 @@ describe("Parser", function () {
     const tokenizer = new Tokenizer(input);
     const parser = new Parser(tokenizer);
     const expect = 42;
+    assert.deepEqual([expect], [...parser]);
+  });
+
+  it("parses a list", function() {
+    const input = "()";
+    const tokenizer = new Tokenizer(input);
+    const parser = new Parser(tokenizer);
+    const expect = List.make();
+    // const expect = Bubble.blow();
+    assert.deepEqual([expect], [...parser]);
+  });
+
+  it("parses a list of numbers", function () {
+    const input = "(83 24 3)";
+    const tokenizer = new Tokenizer(input);
+    const parser = new Parser(tokenizer);
+    // const expect = Bubble.blow(83, 24, 3);
+    const expect = List.make(83, 24, 3);
     assert.deepEqual([expect], [...parser]);
   });
 
@@ -85,3 +172,11 @@ describe("Parser", function () {
 //   console.log(expression);
 // }
 
+class NoMatchError extends Error {
+  name = "NoMatchError";
+
+  constructor(token){
+    super("No match for token " +
+      JSON.stringify(token));
+  }
+}
