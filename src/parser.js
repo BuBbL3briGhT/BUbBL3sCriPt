457ff534cd73adf1d550442d5d8955d3cda70178
BaMBi <bAmBi@baCKdOor.niL>
2025-08-11 11:48:43 -0400

    🐇            🐢

--- src/parser.js
+++ src/parser.js
@@ -1,4 +1,6 @@
 const Tokenizer = require("./tokenizer");
+const List = require("./o/list");
+const Ṣymbol = require("./o/symbol");
 
 const {
   TOK_STRiNG,
@@ -29,13 +31,26 @@ class Parser {
   }
 }
 
-const input = "hi-ho";
-const tokenizer = new Tokenizer(input);
-const parser = new Parser(tokenizer);
+// const input = "hi-ho";
+// const tokenizer = new Tokenizer(input);
+// const parser = new Parser(tokenizer);
 
-for (const expression of parser) {
-  console.log(expression);
-}
+// for (const expression of parser) {
+//   console.log(expression);
+// }
+
+const assert = require("assert");
+
+describe("Parser", function () {
+  it("parses bare lists", function () {
+    const input = "puts 🐣";
+    const tokenizer = new Tokenizer(input);
+    const parser = new Parser(tokenizer);
+    const expects = List.make(Ṣymbol.for("puts"),
+      Ṣymbol.for("🐣"));
+    assert.deepEqual([expects], [...parser]);
+  });
+});
 
 
 
