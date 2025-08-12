f5327e1364d1ba1012465fa6ebe46684c5fb19bc
BaMBi <bAmBi@baCKdOor.niL>
2025-08-12 19:35:25 -0400

    🐇   🐢

--- src/lazy_list.js
+++ src/lazy_list.js
@@ -19,23 +19,22 @@ class LazyList extends List {
 
   get oo() {
     if ( !this.isEmpty )
-      this.set("oo", new LazyList(this.itty));
+      this.set({ oo: new LazyList(this.itty) });
 
     return this.oo;
   }
 
   wakeUp() {
     const o = this.itty.next();
-    this.set("o", o.value);
-    this.set("isEmpty", o.done);
+    this.set({ o: o.value, isEmpty: o.done });
   }
 
-  // Sets the value of a property on this object
-  // using Object.defineProperty.
-  set(property, value) {
-     Object.defineProperty(this, property, {
-       value: value
-     });
+  set(props) {
+    for(const prop in props) {
+      Object.defineProperty(this, prop, {
+        value: props[prop]
+      });
+    }
   }
 
   set o(o) {};
@@ -44,3 +43,48 @@ class LazyList extends List {
 }
 
 module.exports = LazyList;
+
+
+  // Sets the value of a property on this object
+  // using Object.defineProperty.
+  // set(props) {
+  //   for(const prop in props) {
+  //     if (Object.hasOwnProperty(this, prop)) {
+  //       Object.defineProperty(this, prop, {
+  //         value: props[prop]
+  //       });
+  //     }
+  //   }
+  // }
+
+  // // Sets the value of a property on this object
+  // // using Object.defineProperty.
+  // set(props) {
+  //   for(const prop in props) {
+  //      console.log(prop, props[prop]);
+  //      Object.defineProperty(this, prop, {
+  //        value: props[prop]
+  //      });
+  //   }
+  // }
+
+  // // Sets the value of a property on this object
+  // // using Object.defineProperty.
+  // set(properties) {
+  //   for(const property in properties) {
+  //      Object.defineProperty(this, property, {
+  //        value: properties[property]
+  //      });
+  //   }
+  // }
+
+  // // Sets the value of a property on this object
+  // // using Object.defineProperty.
+  // set(properties) {
+  //   properties = Object.entries(properties);
+  //   for(const [property, value] of properties) {
+  //      Object.defineProperty(this, property, {
+  //        value: value
+  //      });
+  //   }
+  // }
