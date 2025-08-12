bee02926caa3c6b37b297bbc39198efd3bfca186
BaMBi <bAmBi@baCKdOor.niL>
2025-08-12 18:34:44 -0400

    🐇   🐢

--- src/lazy_list.js
+++ src/lazy_list.js
@@ -7,40 +7,39 @@ class LazyList extends List {
     this.itty = itty;
   }
 
-  set o(o) {};
-  set oo(oo) {}
-
   get isEmpty() {
-    const o = this.itty.next();
-
-    Object.defineProperty(this, "o",
-                           { value: o.value });
-
-    Object.defineProperty(this, "isEmpty",
-                           { value: o.done });
-
+    this.wakeUp();
     return this.isEmpty;
   }
 
   get o() {
-    const o = this.itty.next();
-
-    Object.defineProperty(this, "isEmpty",
-                           { value: o.done });
-
-    Object.defineProperty(this, "o",
-                           { value: o.value });
-
+    this.wakeUp();
     return this.o;
   }
 
   get oo() {
-    if (!this.isEmpty)
-      Object.defineProperty(this, "oo",
-          { value: new LazyList(this.itty) });
+    if ( !this.isEmpty )
+      this.set("oo", new LazyList(this.itty));
 
     return this.oo;
   }
+
+  wakeUp() {
+    const o = this.itty.next();
+    this.set("o", o.value);
+    this.set("isEmpty", o.done);
+  }
+
+  // Sets the value of a property on this object
+  // using Object.defineProperty.
+  set(property, value) {
+     Object.defineProperty(this, property, {
+       value: value
+     });
+  }
+
+  set o(o) {};
+  set oo(oo) {}
 
 }
 
--- test/lazy_list.js
+++ test/lazy_list.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const LazyList = require("../src/lazy_list.js");
 
 describe("LazyList", function () {
-  it("a lazzzy list", function () {
+  it("is a lazzzy list", function () {
     const expects = [1, 2, 3];
     const itty = expects[Symbol.iterator]();
     const lazy = new LazyList(itty);
