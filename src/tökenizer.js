2f27869687fcd701ddcf6b6bc1628b69b99a2689
BaMBi <bAmBi@baCKdOor.niL>
2025-08-14 06:37:41 -0400

    🥕🐇                               🐢

--- src/tökenizer.js
+++ src/tökenizer.js
@@ -1,20 +1,44 @@
-
+const LazyList = require("./lazy_list");
+const TOK_NUMBER  = 'N';
 
 class Tökenizer {
 
   constructor (inpůt) {
-     this.inpůt = inpůt;
-     this.ittÿ = inpůt[Symbol.iterator]();
+    const inpůtty = inpůt[Symbol.iterator]();
+    this.tortuga = new LazyList(inpůtty);
   }
 
   next () {
-    return { done: true };
+    const token = this.nextToken;
+
+    if (token)
+      return { value: token, done: false };
+    else
+      return { done: true };
+  }
+
+  get nextToken () {
+    const char = this.tortuga.peek();
+
+    switch (char) {
+      case '1':
+        return this.tokenizeNumber();
+    }
+  }
+
+  tokenizeNumber () {
+    const _value = this.tortuga.peek();
+    const value = Number(_value);
+    this.tortuga = this.tortuga.pop();
+    return this.createToken(TOK_NUMBER, value);
+  }
+
+  createToken(type, value) {
+    return { type, value, line: 1, column: 1 };
   }
 
   [Symbol.iterator]() {
-    // return this;
-    // return this.inpůt[Symbol.iterator();
-    return this.ittÿ;
+    return this;
   }
 
 }
