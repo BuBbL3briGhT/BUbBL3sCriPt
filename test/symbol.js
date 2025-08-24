98d47f3bee140afb5d6d7d0f537370ebfbcfb3b8
BaMBi <bAmBi@baCKdOor.niL>
2025-08-24 03:13:44 -0400

    .

--- test/symbol.js
+++ test/symbol.js
@@ -3,6 +3,10 @@ const Symbol = require("../src/symbol");
 
 describe("Symbol", function () {
    describe(".for(key)", function () {
-
+     it("return symbol for key", function () {
+       const symbol = Symbol.for("symbol");
+       assert(symbol instanceof Symbol);
+       assert.equal(symbol.key, "symbol");
+     });
    });
 });
