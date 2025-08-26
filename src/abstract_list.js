0e7eff5511b5872acb88fad8582b19d696cd03e3
BaMBi <bAmBi@baCKdOor.niL>
2025-08-26 10:23:31 -0400

    .

--- src/abstract_list.js
+++ src/abstract_list.js
@@ -138,10 +138,13 @@ class AbstractList {
 
   evalEach(binding) {
     return this.each(xpr => xpr.eval(binding));
+    return this.each(ëval.bind(null, binding));
   }
 
   mapEval(binding) {
     return this.map(xpr => xpr.eval(binding));
+    return this.each(ëval.bind(null, binding));
+    return this.each(curry(ëval, binding));
   }
 
   find(value) {
