ee375f745904a624813344692a14cc49e204ff10
BaMBi <bAmBi@baCKdOor.niL>
2025-08-26 08:38:39 -0400

    .

--- src/eval.js
+++ src/eval.js
@@ -139,99 +139,99 @@ function ëval(binding, expression) {
 //   }
 // }
 
-function ēval(binding, expression) {
-  switch (expression.constructor) {
-    case Function:
-      return expression.call(binding);
-    default:
-      return expression.eval(binding);
-  }
-}
+// function ēval(binding, expression) {
+//   switch (expression.constructor) {
+//     case Function:
+//       return expression.call(binding);
+//     default:
+//       return expression.eval(binding);
+//   }
+// }
 
-function evalList(binding, list, callStack=List.make()) {
-  const [head, tail] = list.plop();
-  const { file, line, column } = list;
+// function evalList(binding, list, callStack=List.make()) {
+//   const [head, tail] = list.plop();
+//   const { file, line, column } = list;
 
-  callStack = callStack.push({
-    head, file, line, column
-  });
+//   callStack = callStack.push({
+//     head, file, line, column
+//   });
 
-  switch (head.constructor) {
-    case Ṣymbol:
-      const headValue = head.eval(binding);
-      return tail.push(headValue).eval();
-    case List:
-      const headValue = ëvalList(binding, head);
-      return ëvalList(binding, tail.push(headValue));
-    case Fn:
-      return head.invoke(tail.mapEval(binding));
-    case Function:
-      return head.call(binding, tail);
-    case Macro:
-      const expanded = head.expand(tail);
-      throw new MacroExpanded(expanded);
-    default:
-      return undefined;
-  }
+//   switch (head.constructor) {
+//     case Ṣymbol:
+//       const headValue = head.eval(binding);
+//       return tail.push(headValue).eval();
+//     case List:
+//       const headValue = ëvalList(binding, head);
+//       return ëvalList(binding, tail.push(headValue));
+//     case Fn:
+//       return head.invoke(tail.mapEval(binding));
+//     case Function:
+//       return head.call(binding, tail);
+//     case Macro:
+//       const expanded = head.expand(tail);
+//       throw new MacroExpanded(expanded);
+//     default:
+//       return undefined;
+//   }
 
 
-  }
-  if (head instanceof Ṣymbol) {
-    if (head.callPattern == 1) {
-      //  x or x/x or x.x/x
-      let q = ëval(bnd, s);
-      if (q != s)
-        return ëval(bnd,
-          list.pop().push(q));
-      else
-        return list;
-    } else /* send */ {
-      // console.log("list", xpr);
-      // call pattern 2
-      // x.x or x.x.x or x.x...
-      let params;
-      // throw new Error("test error");
-      let resolvedRoot = s.resolveRoot(bnd)
-      if (resolvedRoot === undefined) {
-        // consola.registro(s + " is undefined");
-        // const error = new BubbleScriptError(s + ` is undefined. file: ${s.file}, line: ${s.line}, column: ${s.column}`);
-        const error = new
-          BubbleScriptError(s +
-            ` is not defined.`);
-        error.stack = "";
-        throw error;
-      }
-      try {
-        if (list.rest) {
-          params = list.rest;
-          let splits = params.split(sAmp);
-          if (splits.count() > 1) {
-            params = ëval(bnd, splits.rest.head.head);
-            params = params.conj(splits.first.mapEval(bnd));
-          } else {
-            params = params.mapEval(bnd);
-          }
-        }
+//   }
+//   if (head instanceof Ṣymbol) {
+//     if (head.callPattern == 1) {
+//       //  x or x/x or x.x/x
+//       let q = ëval(bnd, s);
+//       if (q != s)
+//         return ëval(bnd,
+//           list.pop().push(q));
+//       else
+//         return list;
+//     } else /* send */ {
+//       // console.log("list", xpr);
+//       // call pattern 2
+//       // x.x or x.x.x or x.x...
+//       let params;
+//       // throw new Error("test error");
+//       let resolvedRoot = s.resolveRoot(bnd)
+//       if (resolvedRoot === undefined) {
+//         // consola.registro(s + " is undefined");
+//         // const error = new BubbleScriptError(s + ` is undefined. file: ${s.file}, line: ${s.line}, column: ${s.column}`);
+//         const error = new
+//           BubbleScriptError(s +
+//             ` is not defined.`);
+//         error.stack = "";
+//         throw error;
+//       }
+//       try {
+//         if (list.rest) {
+//           params = list.rest;
+//           let splits = params.split(sAmp);
+//           if (splits.count() > 1) {
+//             params = ëval(bnd, splits.rest.head.head);
+//             params = params.conj(splits.first.mapEval(bnd));
+//           } else {
+//             params = params.mapEval(bnd);
+//           }
+//         }
 
-        return resolvedRoot[s.fn](...params);
-      } catch (e) {
-        // console.log(s.fn);
-        throw e;
-      }
-    }
-  } else if (s instanceof List) {
-    return ëval(bnd,
-      list.pop().push(ëval(bnd, s)))
-  } else if (s instanceof Fn) {
-    return s.invoke(list.pop().mapEval(bnd));
-  } else if (s instanceof Function) {
-    return s.call(bnd, list.pop());
-  } else if (s instanceof Macro) {
-    let expanded = s.expand(list.pop());
-    throw new MacroExpanded(expanded);
-  } else {
-    return undefined;
-  }
-}
+//         return resolvedRoot[s.fn](...params);
+//       } catch (e) {
+//         // console.log(s.fn);
+//         throw e;
+//       }
+//     }
+//   } else if (s instanceof List) {
+//     return ëval(bnd,
+//       list.pop().push(ëval(bnd, s)))
+//   } else if (s instanceof Fn) {
+//     return s.invoke(list.pop().mapEval(bnd));
+//   } else if (s instanceof Function) {
+//     return s.call(bnd, list.pop());
+//   } else if (s instanceof Macro) {
+//     let expanded = s.expand(list.pop());
+//     throw new MacroExpanded(expanded);
+//   } else {
+//     return undefined;
+//   }
+// }
 
 module.exports = { ėval, ëval };
