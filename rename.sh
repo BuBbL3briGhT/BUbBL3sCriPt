6bf6aaa0b34892f9ecd62cf1f6d34d636771c975
BaMBi <bAmBi@baCKdOor.niL>
2025-08-28 18:48:25 -0400

    . diversión bajo el sol

--- rename.sh
+++ rename.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/env bash
+
+# rename original nuevo [dir]
+
 # orginal_texto = $1;
 # nuevo_texto
 # echo $1 $2
-# git grep -l $1 | xargs sed -i -e 's/$1/$2/g'
+# git grep -l $1 | xargs sed -i -e "s/$1/$2/g"
 
 # File renaming.
 files=$(git ls-tree --full-tree --name-only -r HEAD)
@@ -14,7 +17,12 @@ files=$(echo "$files" | grep "$1")
 for file in $files; do
   echo "Renaming: $file"
   # get new path
+  new_path=$(echo "$file" | sed "s/$1/$2/g")
+  echo "$new_path"
   # get dir
+  # directory_path=$(dirname "$new_path")
+  # mkdir -p "$directory_name"
   # rename old to new
+  # mv "$file" "$new_path"
 done
 # IFS=$" \t\n"
