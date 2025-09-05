4538597fcfcb1ea267d2adbd5e245671071bebd2
BaMBi <bAmBi@baCKdOor.niL>
2025-09-05 14:20:32 -0400

    .

--- src/root_binding.js
+++ src/root_binding.js
@@ -132,10 +132,7 @@ const rootBinding = {
 
   let: specialForm(function(list) {
     const [params, body] = list.plop();
-    // console.log({params});
     const binding = Object.create(this);
-    // const binding = createBinding
-    // const paramsList = params.toList();
     params.toList().partition(2)
       .each(([llave, valor]) => {
         binding[llave] =
