d644dc75211d2d01491e9c81fb674b0c1f283d75
BaMBi <bAmBi@baCKdOor.niL>
2025-09-06 16:30:26 -0400

    .

--- src/create_binding.js
+++ src/create_binding.js
@@ -16,47 +16,49 @@
 
      const sAmp = Ṣymbol.for("&");
 
-// Applys the keys and the values to the
-// binding based on order and position.
-// Binding will be modified.
-function applyArguments(binding, keys, vals) {
-  if (keys instanceof Vector)
-    keys = keys.toList();
-  if (vals instanceof Vector)
-    vals = vals.toList();
+  // Applys the keys and the values to the
+  // binding based on order and position.
+  // Binding will be modified.
+  function applyArguments
+        (binding, keys, vals)
+  {
+        if (keys instanceof Vector)
+          keys = keys.toList();
+        if (vals instanceof Vector)
+          vals = vals.toList();
 
-  // console.log("keys", keys);
-  // console.log("vals", vals);
-  while (!keys.isEmpty && !vals.isEmpty) {
-    let key = keys.first;
-    let val = vals.first;
+    // console.log("keys", keys);
+    // console.log("vals", vals);
+    while (!keys.isEmpty && !vals.isEmpty) {
+      let key = keys.first;
+      let val = vals.first;
 
-    if (key == sAmp) {
-      binding[keys.next] = vals;
-      return binding;
+      if (key == sAmp) {
+        binding[keys.next] = vals;
+        return binding;
+      }
+
+      if(val == sAmp) {
+        applyArguments(binding, keys, vals.next)
+        return binding;
+      }
+
+      switch (key.constructor) {
+        case List:
+        case Vector:
+          applyArguments(binding, key, val);
+          break;
+        case Ṣymbol:
+          binding[key.toString()] = val;
+          break;
+        default:
+          throw Error("Invalid parameter type: " + key.constructor );
+      }
+
+      keys = keys.rest;
+      vals = vals.rest;
     }
-
-    if(val == sAmp) {
-      applyArguments(binding, keys, vals.next)
-      return binding;
-    }
-
-    switch (key.constructor) {
-      case List:
-      case Vector:
-        applyArguments(binding, key, val);
-        break;
-      case Ṣymbol:
-        binding[key.toString()] = val;
-        break;
-      default:
-        throw Error("Invalid parameter type: " + key.constructor );
-    }
-
-    keys = keys.rest;
-    vals = vals.rest;
   }
-}
 
 // Creates a binding object for a function or
 // macro.
