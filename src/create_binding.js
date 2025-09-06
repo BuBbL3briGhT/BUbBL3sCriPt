8b113c4a966cb58d9703091e29cc021d404bc673
BaMBi <bAmBi@baCKdOor.niL>
2025-09-06 16:32:07 -0400

    .

--- src/create_binding.js
+++ src/create_binding.js
@@ -4,6 +4,7 @@
    *   File: src/create_binding.js    *
    *   Date: September, 6th 2025      *
    *   Library: Bubblescript          *
+   *   version: 0.0.🦤.🍌             *
    *   Version: 0.0.16                *
    *   Author(s): BaMbii              *
    *                                  *
