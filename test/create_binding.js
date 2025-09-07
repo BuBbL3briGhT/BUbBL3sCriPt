96d0ffd3c80387c99d419b84e1e18ad410ffcd42
BaMBi <bAmBi@baCKdOor.niL>
2025-09-06 21:14:02 -0400

    .

--- test/create_binding.js
+++ test/create_binding.js
@@ -5,10 +5,7 @@ const Ṣymbol = require("../src/symbol.js");
 
 const ṣ = Ṣymbol.for.bind(Ṣymbol);
 
-  const falsie = 0,
-         falsy = 0,
-         falso = 0,
-         falsa = 0;
+   const falso = 0, falsa = 0;
 
 describe("createBinding", function () {
 
