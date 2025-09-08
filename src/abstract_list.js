637f2b95e2ff4bb3b1ada0432ddaaa15eff1182e
BaMBi <bAmBi@baCKdOor.niL>
2025-09-07 20:44:06 -0400

    . count

--- src/abstract_list.js
+++ src/abstract_list.js
@@ -52,9 +52,7 @@ class AbstractList {
       this.pop().last : this.peek(); }
 
   count() {
-    return this.reduce((count) => {
-      return ++count;
-    }, 0);
+    return this.reduce(count => ++count, 0);
   }
 
   map(fn) {
