9367b56378e131e507cefe63487e21b9452196a9
BaMBi <bAmBi@baCKdOor.niL>
2025-09-12 13:20:41 -0400

    📝

--- src/eval.js
+++ src/eval.js
@@ -28,25 +28,48 @@ function evalExpression(expresíon) {
   } else return expresíon;
 }
 
+// Esta es una pequeña función peculiar que pretende
+// simplificar, pero probablemente complique un poco
+// las cosas. La dejo por ahora, pero probablemente
+// exista una forma mucho más clara y sencilla de
+// expresarlo. No me gustan mucho los nombres de
+// función largos, ya que son una forma de código
+// fétido. Básicamente, solo sirve como
+// representación de una idea y quizás para
+// proporcionar claridad. Esta función espera un
+// enlace y una expresión; luego, reemplaza la
+// propiedad especial reservada de la pila con una
+// nueva pila que tiene la expresión insertada en la
+// parte superior. Luego, invoca la función
+// devolviendo el enlace y la expresión como
+// parámetros; tras almacenar el valor devuelto,
+// restablece la propiedad especial reservada de la
+// pila en el enlace al valor anterior y, finalmente,
+// devuelve el valor recibido de la llamada a la
+// función. #AbstracciónConFiltración
+// #ExcesoDeAcciones #NombreDeFunciónLargo
+//
 // This right here is a crazy little function that is
 // meant to simplify, but probably makes things a bit
 // more complex. Leaving it in for now but there is
-// probably a much cleaner and simple way to express
-// this.  I'm not a huge fan of long function names
-// as they are a form of code smell. just serving as
-// a representation of an idea and maybe providing a
-// notion of clarity.  This function expects a
-// binding and an expression, it then replaces the
-// special reserved stack property with a new stack
-// that has the expresion pushed on top.  it then
-// invokes the function passing back the bimding and
-// expression as parameters, after storing the
-// returned value, it resets the special reserved
-// stack property in the binding to the previously
-// value and finally, returns the value it recieved
-// from the function call.  #leakyAbstraction
-// #overKill #longFunctionName
-
+// probably a much cleaner and simpler way to express
+// this. I'm not a huge fan of long function names
+// as they are a form of code smell. It's mostly just
+// serving as a representation of an idea and maybe
+// providing a notion of clarity. This function
+// expects a binding and an expression, it then
+// replaces the special reserved stack property with
+// a new stack that has the expresion pushed on top.
+// It then invokes the function passing back the
+// binding and expression as parameters, after
+// storing the returned value, it resets the special
+// reserved stack property in the binding to the
+// previously value and finally, returns the value it
+// recieved from the function call.
+// #leakyAbstraction #overKill #longFunctionName
+//
+// This comment was tanslated by Google
+// (https://www.google.com/search?q=translate)
 function pilaDeLlamadasEmpujarEval(vínculo,
   expresíon, funcíon) {
   const __pilaDeLlamadas = vínculo.__pilaDeLlamadas;
