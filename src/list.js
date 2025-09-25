ce43d6ac996651190e3630dcc7737ca355a58395
BaMBi <bAmBi@baCKdOor.niL>
2025-09-25 07:58:15 -0400

    .

--- src/list.js
+++ src/list.js
@@ -140,21 +140,21 @@ class List extends AbstractList {
       // vínculo.__pilaDeLlamadas = __pilaDeLlamadas;
       // return resultado;
     } catch (error) {
-      switch (error.constructor){
-        case ErrorDeFuncíonIndefinida:
-          if (error.__memo) {
-            const memo = error.__memo;
-            error.stack += "\n" + interpolarTrazaPlantilla({
-              fn: this.head,
-              file: memo.file,
-              line: memo.line,
-              column: memo.column
-            });
-          }
-          const { file, line, column } = this;
-          error.__memo = { file, line, column }
-          throw error;
-      }
+      // switch (error.constructor){
+      //   case ErrorDeFuncíonIndefinida:
+      //     if (error.__memo) {
+      //       const memo = error.__memo;
+      //       error.stack += "\n" + interpolarTrazaPlantilla({
+      //         fn: this.head,
+      //         file: memo.file,
+      //         line: memo.line,
+      //         column: memo.column
+      //       });
+      //     }
+      //     const { file, line, column } = this;
+      //     error.__memo = { file, line, column }
+      // }
+      throw error;
     }
   }
 
