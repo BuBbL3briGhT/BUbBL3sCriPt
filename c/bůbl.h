dcb6662ac53b16e23b1f847ab2db61e5606fea84
BaMBi <bAmBi@baCKdOor.niL>
2025-10-01 14:16:13 -0400

    .

--- c/bůbl.h
+++ c/bůbl.h
@@ -1,24 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the structure for a node
-struct Bůbl {
-    int o;
-    struct Bůbl* oo;
-};
+struct Bůbl { int o; struct Bůbl* oo; };
 
-// Function to create a new node
-struct Bůbl* blowBůbl(int o);
-
-struct Bubl* pushBůbl(struct Bůbl head, int o);
-
-struct Bubl* popBůbl(struct Bůbl head);
-
-// Function to insert a node at the end of the list
-void insertEnd(struct Bůbl** head, int o);
-
-// Function to display the linked list
-void displayList(struct Bůbl* head);
-
-// Function to delete a node by value
-void deleteBůbl(struct Bůbl** head, int key);
+struct Bůbl* blow(int);
+struct Bubl* push(struct Bůbl, int);
+struct Bubl* pop(struct Bůbl);
