98582d94e13a51b05794a6acfef79d2f13713911
BaMBi <bAmBi@baCKdOor.niL>
2025-10-01 06:32:44 -0400

    .

--- c/linked_list.c
+++ c/linked_list.c
@@ -9,7 +9,8 @@ struct Node {
 
 // Function to create a new node
 struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* newNode =
+      (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
     newNode->next = NULL;
     return newNode;
