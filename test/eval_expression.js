cb33c063b8e4196c897ad45b41af1e0e7985926c
BaMBi <bAmBi@baCKdOor.niL>
2025-11-08 03:12:25 -0500

    . Second test passing. Test evalExpression evaluates a symbol.
      bubblescript 🍀 npx mocha test/eval_expression.js
    
        evalExpression
          ✔ evaluates an expression
          ✔ evaluates a Ṣymbol
    
        2 passing (20ms)

--- test/eval_expression.js
+++ test/eval_expression.js
@@ -1,10 +1,18 @@
 const assert = require("assert");
 const { evalExpression } = require("../src/eval");
+const Ṣymbol = require("../src/symbol");
 // const { rootBinding } = require("../src/rootBinding");
 
 describe("evalExpression", function () {
   it("evaluates an expression", function () {
     const result = evalExpression(null, 1);
     assert.equal(result, 1);
+  });
+
+  it("evaluates a Ṣymbol", function () {
+    const binding = { a: "Apple" };
+    const symbol = Ṣymbol.for("a");
+    const result = evalExpression(binding, symbol);
+    assert.equal(result, "Apple");
   });
 });
