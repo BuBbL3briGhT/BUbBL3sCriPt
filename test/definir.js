41917656751ec24a1b8b2cef11ed4548de859fc9
BaMBi <bAmBi@baCKdOor.niL>
2025-11-08 18:10:04 -0500

    . setup looks good. No definir function
    ibubblescript 🍀 npx mocha test/definir.jso
    
      definir
          1) sets a constant
    
            0 passing (28ms)
              1 failing
    
                1) definir
                       sets a constant:
                            The function "definir" is not defined.
                                  en (::)

--- test/definir.js
+++ test/definir.js
@@ -1,29 +1,19 @@
-const assert = require("assert");
-const { ėval } = require("../src/eval");
-// const { rootBinding, List } =
-//   require("../src/BubbleScript");
 
-const rootBinding = require("../src/root_binding");
-const List = require("../src/list");
+                                        const assert = require("assert");
+                                      const { ėval } = require("../src/eval");
+                                   const rootBinding = require("../src/root_binding");
+                                          const List = require("../src/list");
 
-describe("definir", function () {
-  it.only("sets a constant", function () {
-    // const vínculo = Object.create(rootBinding);
-    const vínculo = {
-      define: function() {
-        console.log("Hola Mama!");
-      }
-    };
-    console.log({vínculo});
-    const result =
-      ėval(vínculo, "define 🍎 \"apple\"");
-    console.log({vínculo});
+  describe("definir", function () {
+    it.only("sets a constant", function () {
+
+      const vínculo = Object.create(rootBinding);
+
+        ėval(vínculo, "definir 🍎 \"apple\"");
+
+      const result = ėval(vínculo, "🍎");
+
+        assert.equal(result, "apple");
+
+    });
   });
-});
-
-// describe("define", function () {
-//   it.only("sets a constant", function () {
-//     rootBinding.define.call(rootBinding,
-//       Symbol
-//   });
-// });
