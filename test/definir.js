aa05e08bccaf1631cc1f97a95fb7ca4eed0a4582
BaMBi <bAmBi@baCKdOor.niL>
2025-11-11 20:49:21 -0500

    🈯️ Izinguqulo zesiSpanishi nesiZulu ze-assert.
    🈯️ Traducciones al español y al zulú de assert.
    
    🈯️ Spanish and Zulu translations for assert.

--- test/definir.js
+++ test/definir.js
@@ -6,38 +6,45 @@
                                           const List = require("../src/list");
                                             const Fn = require("../src/fn");
 
-                                          const zulu = it;
+                                          const yona = it; // Zulu
+                                          const ella = it; // Española
+
+                                     const describir = describe; // Española
+                                         const chaza = describe; // Española
+
+                                       const afirmar = assert; // Española
+                                        const qinisa = assert; // Zulu
 
   // Describe define.
-  describe("definir", function () {
+  describir("definir", function () {
 
     // It sets a value.
-    it("establece un valor", function () {
+    ella("establece un valor", function () {
 
       const vínculo = Object.create(rootBinding);
 
-        assert.equal(ėval(vínculo, "🍎"), undefined);
+        afirmar.equal(ėval(vínculo, "🍎"), undefined);
 
-      ėval(vínculo, "definir 🍎 \"apple\"");
+      ėval(vínculo, "definir 🍎 \"manzana\"");
 
-        const result = ėval(vínculo, "🍎");
+        const resultado = ėval(vínculo, "🍎");
 
-      assert.equal(result, "apple");
+      afirmar.equal(resultado, "manzana");
 
     });
 
     // It doesn't define a value if it was already
     // defined (value is constant).
-    it("no define un valor si ya estaba definido "+
+    ella("no define un valor si ya estaba definido "+
        "(el valor es constante)", function () {
 
       const vínculo = Object.create(rootBinding);
 
-        ėval(vínculo, "definir 🍎 \"apple\"");
+        ėval(vínculo, "definir 🍎 \"manzana\"");
 
-      assert.throws(function () {
+      afirmar.throws(function () {
 
-         ėval(vínculo, "definir 🍎 \"apple\"");
+         ėval(vínculo, "definir 🍎 \"manzana\"");
 
       }, Error);
 
@@ -46,7 +53,7 @@
     // It creates and sets a function when passed a
     // list as the first parameter, and uses the
     // remainder of the list as the body.
-    zulu("Idala futhi isetha umsebenzi lapho "+
+    yona("Idala futhi isetha umsebenzi lapho "+
          "idlulisa uhlu njengepharamitha yokuqala, "+
          "futhi isebenzisa ingxenye esele yohlu "+
          "njengomzimba."
@@ -59,11 +66,11 @@
 
        ėval(vínculo, "definir (🐟) (💜)");
 
-         assert.equal(vínculo["🐟"].constructor, Fn);
+         qinisa.equal(vínculo["🐟"].constructor, Fn);
 
        ėval(vínculo, "(🐟)");
 
-         assert.ok(vínculo["💜"].called, "💜 should have been called");
+         qinisa.ok(vínculo["💜"].called, "💜 should have been called");
 
     });
 
