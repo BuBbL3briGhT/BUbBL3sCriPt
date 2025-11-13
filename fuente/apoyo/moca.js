0448ba394b68a83e5bdf194181d22eb1001d4b12
BaMBi <bAmBi@baCKdOor.niL>
2025-11-13 08:34:52 -0500

    . Ok, this is the problematic code. Prototyping a function, i guess you can't do it that way. Anyway, will look into it further, this was for support other languages, not the only way, nor probably the best way to to this. Was kinda clever though.

--- fuente/apoyo/moca.js
+++ fuente/apoyo/moca.js
@@ -5,8 +5,10 @@
 
         const Moca = Objeto.crear(Mocha);
 
-         const ella = Objeto.crear(Moca.it);
-    const describir = Objeto.crear(Moca.describe);
+         // const ella = Objeto.crear(Moca.it);
+    // const describir = Objeto.crear(Moca.describe);
+         const ella = Moca.it;
+    const describir = Moca.describe;
 
     ella.saltar = Mocha.it.skip;
       ella.sola = Mocha.it.only;
