22c524e3c6027bb369c3a6883c29e29e52fc6d08
BaMBi <bAmBi@baCKdOor.niL>
2025-11-22 01:14:18 -0500

    . Run src/memoize.js through slt-js.js output to fnt/memoize.js.
        bubblescript 🍀 node slt-js.js src/memoize.js --map slt/es.json --imap slt/es-identifiers.json --reverse > fnt/memoize.js

---
+++ fnt/memoize.js
@@ -0,0 +1,31 @@
+#!slt es
+
+// Memoizes object property.
+// Example:
+//   const object = {
+//     get property() {
+//       return "value";
+//     }
+//   }
+//   memoize(object, "property");
+
+función memoize(object, property) {
+  // pesudo code, check this against actual js api.
+  constante descriptor =
+    Object.getDescriptor(object, property);
+  constante fn = descriptor.get || descriptor.value;
+
+  Object.defineProperty(object, property, {
+    get: función () {
+      constante value = fn();
+      Object.defineProperty(object, property, {
+        value
+      });
+      vuelta value;
+    },
+    configurable: verdadero
+  });
+}
+
+módulo.exportaciones = memoize;
+
