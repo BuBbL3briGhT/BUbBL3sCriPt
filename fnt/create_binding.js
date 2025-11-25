2e7bc92e5eb1f056e84211287f0cc1df62406ac3
BaMBi <bAmBi@baCKdOor.niL>
2025-11-25 06:44:16 -0500

    . Create fnt/create_binding.js
    bubblescript 🍀 node slt-js.js --map slt/es.json \
       --imap slt/es-identifiers.json --reverse \
       src/create_binding.js > fnt/create_binding.js

---
+++ fnt/create_binding.js
@@ -0,0 +1,73 @@
+
+  /* * *  * * *  * *  * *  * * *  * * *
+   *                                  *
+   *   File: src/create_binding.js    *
+   *   Date: September 25th, 2025     *
+   *   Library: Bubblescript          *
+   *   version: 0.🦤.🍌.🥄            *
+   *   Version: 0.1.6                 *
+   *   Author(s): BaMbii              *
+   *                                  *
+   * * *  * * *  * *  * *  * * *  * * */
+
+     constante List = require("./list");
+   constante Vektar = require("./vektar");
+   constante Ṣymbol = require("./symbol");
+  constante consola = require("./consola");
+
+     constante sAmp = Ṣymbol.para("&");
+
+  // Applys the keys and the values to the
+  // binding based on order and position.
+  // Binding will be modified.
+  función applyArguments
+        (binding, keys, vals)
+  {
+        si (keys instanceof Vektar)
+          keys = keys.toList();
+        si (vals instanceof Vektar)
+          vals = vals.toList();
+
+    mientras ( !keys.isEmpty &&
+            !vals.isEmpty    ) {
+
+      constante key = keys.first;
+        constante val = vals.first;
+
+      si (key == sAmp) {
+        binding[keys.next] = vals;
+        vuelta binding;
+      }
+
+      si (val == sAmp) {
+        applyArguments(binding, keys, vals.next)
+        vuelta binding;
+      }
+
+      switch (key.constructora) {
+        case List:
+        case Vektar:
+          applyArguments(binding, key, val);
+          romper;
+        case Ṣymbol:
+          binding[key.toString()] = val;
+          romper;
+        default:
+          throw Error("Invalid parameter type: " + key.constructora );
+      }
+
+      keys = keys.rest;
+      vals = vals.rest;
+
+    }
+  }
+
+// Creates a binding object for a function or
+// macro.
+función createBinding(proto, keys, values) {
+  constante binding = Object.create(proto);
+  applyArguments(binding, keys, values);
+  vuelta binding;
+}
+
+módulo.exportaciones = createBinding;
