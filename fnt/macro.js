3390cce59ec59cac8aef33ba4dce839ed9f60e7e
BaMBi <bAmBi@baCKdOor.niL>
2025-12-01 21:52:57 -0500

    . Traducir fnt/macro.js al española.

--- fnt/macro.js
+++ fnt/macro.js
@@ -1,44 +1,41 @@
-constante createBinding = require("./create_binding");
-constante List = require("./list");
+constante crearEnlace = requerir("./crear_enlace");
+constante Lista = requerir("./lista");
 
 clase Macro {
-  constructora(binding, params, body, opts={}) {
-    esta.binding = binding;
-    esta.params = params.toList();
-    esta.body = body;
-    esta.name = opts.name;
+  constructora
+  (enlace, parámetros, cuerpo, opciones={}) {
+    Objeto.asignar(esta, { enlace, parámetros,
+      cuerpo, nombre }, opciones);
   }
 
-  expand(params) {
-    // console.log("params", params);
-    // console.log("this.params", this.params);
-    deja binding = createBinding(esta.binding,
-      esta.params, params);
+  expandir(parámetros) {
+    deja enlace = crearEnlace(esta.enlace,
+      esta.parámetros, parámetros);
 
-    vuelta esta.body.mapEval(binding);
+    vuelta esta.cuerpo.evaluaciónDeMapas(enlace);
   }
 
-  // this is a guess but seems about right, will
-  // need testing and proof to be sure. a little
-  // preplext as to why the current binding doesn't
-  // need to be passed int the expansion. this may
-  // be an over sight, and it maybe be correct as
-  // is.
-  call(binding, params) {
-    constante expanded = esta.expand(params);
-    throw nuevo MacroExpanded(expanded);
+  // Esto es una suposición, pero parece correcto; se
+  // necesitarán pruebas para confirmarlo. Tengo
+  // cierta duda sobre por qué no es necesario
+  // pasar la vinculación actual a la ex  pansión.
+  // Puede que sea un descuido, y puede que sea
+  // correcto tal como está.
+  llamar(enlace, parámetros) {
+    constante expandida = esta.expandir(parámetros);
+    tirar nuevo MacroExpandida(expandida);
   }
 
-  toString() {
-    vuelta "(macro " + esta.args.toString() +
-      esta.body.toString() + ")";
+  encordar() {
+    vuelta "(macro " + esta.parámetros.encordar() +
+      esta.cuerpo.encordar() + ")";
   }
 }
 
-clase MacroExpanded {
-  constructora(expanded) {
-    esta.expanded = expanded;
+clase MacroExpandida {
+  constructora(expandida) {
+    esta.expandida = expandida;
   }
 }
 
-módulo.exportaciones = { Macro, MacroExpanded };
+módulo.exportaciones = { Macro, MacroExpandida };
