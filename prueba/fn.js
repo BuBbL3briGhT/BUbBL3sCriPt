773584018c2d9648245130ad4dee2e8cc4d29550
BaMBi <bAmBi@baCKdOor.niL>
2025-12-17 15:58:13 -0500

    . prueba/fn.js

--- prueba/fn.js
+++ prueba/fn.js
@@ -1,15 +1,16 @@
 const assert = require("assert");
-const Ṣymbol = require("../src/symbol");
 const Fn = require("../src/fn");
-const List = require("../src/list");
+const { List } = require("../src/list");
+const Ṣymbol = require("../src/symbol");
 
 describe("Fn", function () {
   describe("toString", function () {
     it("returns function as a string", function ()  {
-      let a = Ṣymbol.for("a");
-      let b= Ṣymbol.for("b");
-      let fn = new Fn({}, List.blow(a, b), List.blow(a));
-      assert.equal(fn.toString(), "(fn (a b) a)");
+      const a = Ṣymbol.for("a");
+      const b = Ṣymbol.for("b");
+      const fn = new Fn({}, List.make(a, b),
+        List.make(a));
+      assert.equal(fn.toString(), "((fn a b) a)");
     });
   });
 });
