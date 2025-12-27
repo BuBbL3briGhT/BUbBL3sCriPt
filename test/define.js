05221d780758dee35612874235c23fc7a3ac6198
BaMBi <bAmBi@baCKdOor.niL>
2025-12-27 08:00:30 -0500

    . Fix formatting for text output.

--- test/define.js
+++ test/define.js
@@ -6,6 +6,35 @@ import { rootBinding } from "../src/binding.js";
 import { List } from "../src/list.js"
 import Fn from "../src/fn.js";
 
+import { it as _it } from "mocha";
+
+
+// Simple function that use a regex replacement to
+// remove newlines and extra spaces and replace with
+// a single space.
+//
+// Example:
+//
+//     const value = `Hello, how are you doing
+//                    today my friend?`;
+//
+//     console.log(value);
+//
+//     Hello, how are you doing
+//                    today my friend?
+//
+//     console.log(formatString(value));
+//
+//     Hello, how are you doing today my friend?";
+//
+function formatString(string) {
+  return string.replace(/\W+/g, " ");
+}
+
+function it(title, ...etc) {
+  return _it(formatString(title), ...etc);
+}
+
 
 // Describe define.
 describe("define", function () {
@@ -14,17 +43,17 @@ describe("define", function () {
 
     const binding = Object.create(rootBinding);
 
-      assert.equal(ėval(binding, "🍎"), undefined);
+    assert.equal(ėval(binding, "🍎"), undefined);
 
     ėval(binding, `define 🍎 "apple"`);
 
-      const result = ėval(binding, "🍎");
+    const result = ėval(binding, "🍎");
 
     assert.equal(result, "apple");
 
   });
 
-  it(`doesn't define a value if it was already \
+  it(`doesn't define a value if it was already
       defined (value is constant).`, function () {
 
     const binding = Object.create(rootBinding);
@@ -37,8 +66,8 @@ describe("define", function () {
 
   });
 
-  it(`creates and sets a function when passed a \
-      list as the first parameter, and uses the \
+  it(`creates and sets a function when passed a
+      list as the first parameter, and uses the
       remainder of the list as the body.`,
     function () {
 
@@ -59,13 +88,14 @@ describe("define", function () {
   });
 
 
-  it.skip(`extracts all key values from an \
-           ObjectMap in the current binding \
-           with *`, function () {
-    const binding = Object.create(rootBinding);
-    // TODO: Implement test.
-  });
+  // it(
 
+  // it.skip(`extracts all key values from an
+  //          ObjectMap in the current binding
+  //          with *`, function () {
+  //   const binding = Object.create(rootBinding);
+  //   // TODO: Implement test.
+  // });
 
 
 });
