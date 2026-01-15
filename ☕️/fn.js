7e3807bc017937f20ae9b1d40866a283a87f46b9
BaMBi <bAmBi@baCKdOor.niL>
2026-01-15 06:29:39 -0500

    .

--- ☕️/fn.js
+++ ☕️/fn.js
@@ -25,8 +25,8 @@ export default class Fn {
         // console.log({params});
         params = ėval.evalParams(binding, params);
         // console.log({params}, "🧀");
-        console.log({params: params.toString()});
-        console.log({fn: fn.toString()});
+        // console.log({params: params.toString()});
+        // console.log({fn: fn.toString()});
         return fn.call(binding, ...params);
     }
 
