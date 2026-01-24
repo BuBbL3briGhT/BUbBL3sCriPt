e9cd0d7736df77421acaf2d0496ad867a415ada0
BaMBi <bAmBi@baCKdOor.niL>
2026-01-23 22:03:39 -0500

    🌟 ¿Soy Ron Burgandy?

--- ☕️/tokenize.js
+++ ☕️/tokenize.js
@@ -169,7 +169,8 @@ export class Tokenizer {
 
       this.step();
 
-    if (this.tortuga["isEmpty?"]) {
+    if (this.tortuga["isEmpty?"] ||
+        symDelims["includes?"](this.tortuga.peek())) {
 
           return this.createToken(
 
