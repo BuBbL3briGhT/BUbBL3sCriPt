c3a90a517f61e23a5d134ef3e6bc7cfe919f891f
BaMBi <bAmBi@baCKdOor.niL>
2026-02-06 17:32:36 -0500

    🧖 Configure prettier.

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,6 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([{
   files: ["**/*.{js,mjs,cjs}"],
   plugins: { js },
-  extends: ["js/recommended"],
+  extends: ["js/recommended", "prettier"],
   languageOptions: { globals: globals.node }
 }]);
