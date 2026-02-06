49ee09c446cdb0a08d0715a806a61b9e8b4f2f17
BaMBi <bAmBi@baCKdOor.niL>
2026-02-06 17:39:15 -0500

    🥲 Linter broken. 🤔
    Bubblescript 🍀 npm run lint
    
    > bubblescript@0.1.6.⛄️.testing lint
    > eslint .
    
    Oops! Something went wrong! :(
    
    ESLint: 9.39.2
    
    TypeError: Plugin "" not found.
        at findPluginConfig (/data/data/com.termux/files/home/bubblescript/node_modules/@eslint/config-helpers/dist/cjs/index.cjs:259:9)
        at /data/data/com.termux/files/home/bubblescript/node_modules/@eslint/config-helpers/dist/cjs/index.cjs:430:25
        at Array.map (<anonymous>)
        at processExtends (/data/data/com.termux/files/home/bubblescript/node_modules/@eslint/config-helpers/dist/cjs/index.cjs:428:36)
        at /data/data/com.termux/files/home/bubblescript/node_modules/@eslint/config-helpers/dist/cjs/index.cjs:504:38
        at Array.flatMap (<anonymous>)
        at processConfigList (/data/data/com.termux/files/home/bubblescript/node_modules/@eslint/config-helpers/dist/cjs/index.cjs:504:20)
        at defineConfig (/data/data/com.termux/files/home/bubblescript/node_modules/@eslint/config-helpers/dist/cjs/index.cjs:540:9)
        at file:///data/data/com.termux/files/home/bubblescript/eslint.config.js?mtime=1770417452943:5:16
        at ModuleJob.run (node:internal/modules/esm/module_job:413:25)

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ['js/recommended', 'prettier'],
+    extends: ['eslint:recommended', 'prettier'],
     languageOptions: { globals: globals.node },
   },
 ]);
